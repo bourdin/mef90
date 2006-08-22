5a03042f5c088eb444e777992017128963d35824
Blaise Bourdin <bourdin@lsu.edu>
2006-08-22 19:00:59 +0000

    [Elast2DASeq] Created project

---
+++ Elast2DASeq/m_Elast2DA_Vars.F90
@@ -0,0 +1,62 @@
+Module m_Elast2DA_Vars
+  Use m_MEF90
+  Use m_Rupt_Struct
+
+  Implicit NONE
+  PRIVATE
+
+
+#include "include/finclude/petsc.h"
+#include "include/finclude/petscvec.h"
+#include "include/finclude/petscvec.h90"
+#include "include/finclude/petscmat.h"
+#include "include/finclude/petscmat.h90"
+#include "include/finclude/petscksp.h"
+#include "include/finclude/petscpc.h"
+#include "include/finclude/petscis.h"
+
+
+  Mat, Public                                           :: MR
+  Vec, Public                                           :: RHS, RHS_Loc
+
+  Vec, Public                                           :: SOL_Dist, SOL_Loc
+  Vec, Public                                           :: SOL_Master
+                                                        
+  Vec, Public                                           :: BC_Dist, BC_Loc
+  Vec, Public                                           :: BC_Master
+
+  Vec, Public                                           :: F_Dist, F_Loc
+  Vec, Public                                           :: F_Master
+
+  KSP, Public                                           :: KSP_MR
+  PC, Public                                            :: PC_MR
+  KSPConvergedReason, Public                            :: KSP_Reason
+
+                                                        
+  Type(Element2D_Scal), Dimension(:), Pointer, Public   :: Elem_db
+  Type(Node2D), Dimension(:), Pointer, Public           :: Node_db
+
+
+  Integer, Public                                       :: GaussOrder=2
+
+  Type(EXO_Geom_Info), Public                           :: Geom
+  Type(Rupt_Params), Public                             :: Params
+  Type(SD_Info), Public                                 :: MySD
+
+  Integer, Public                                       :: iErr
+  Integer, Public                                       :: TimeStep
+
+  Character(len=128), Public                            :: CharBuffer
+
+  PetscLogDouble, Public                                :: TotalTS, TotalTF
+  PetscLogDouble, Public                                :: TotalT
+  PetscLogDouble, Public                                :: InitTS, InitTF
+  PetscLogDouble, Public                                :: AssembTS, AssembTF
+  PetscLogDouble, Public                                :: RHSTS, RHSTF
+  PetscLogDouble, Public                                :: SolveTS, SolveTF
+  PetscLogDouble, Public                                :: ExportTS, ExportTF
+
+  Real(Kind = Kr), Public                               :: Ener_Elast
+  PetscReal, Public, Parameter                          :: VLV = 1.0e+20
+
+End Module m_Elast2DA_Vars
