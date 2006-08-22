9895e7620d8d7e58216de49039d339e62d0134cc
bourdin <bourdin@localhost>
2006-08-22 18:59:49 +0000

    [Elast2DA] Antiplane shear elasticity

--- Elast2DA/Elast2DA.F90
+++ Elast2DA/Elast2DA.F90
@@ -1,18 +1,11 @@
-Program Elast
+Program Elast2DA
 
   Use m_MEF90
   Use m_Rupt_Struct
 
 
-#ifdef PB_2D
-  Use m_Elast2D_Vars
-  Use m_Elast2D_Proc
-  Use m_Elast2D_Debug
-#else
-  Use m_Elast3D_Vars
-  Use m_Elast3D_Proc
-  Use m_Elast3D_Debug
-#endif
+  Use m_Elast2DA_Vars
+  Use m_Elast2DA_Proc
   Implicit NONE
 
 #include "include/finclude/petsc.h"
@@ -44,53 +37,29 @@ Program Elast
   Call VecSet(SOL_Dist, 1.0_Kr, iErr)
 
 
-!!$  Call GenVect(Geom, MySD, Node_db, Elem_db, SOL_Dist)
-!!$  Call VecGhostUpdateBegin(SOL_Dist, INSERT_VALUES, SCATTER_FORWARD, iErr)
-!!$  Call VecGhostUpdateEnd(SOL_Dist, INSERT_VALUES, SCATTER_FORWARD, iErr)
-!!$  Call Calc_Ener(SOL_Loc, Geom, Params, Elem_db, Node_db, MySD,            &
-!!$       & 1.0_Kr, Ener_Elast) 
-!!$  TimeStep = 1
-!!$  Call Export()
-!!$
-!!$     STOP
 
-
-#ifdef MEF90_TIMING
   Call PetscGetTime(InitTF, iErr)
   If (MEF90_MyRank == 0) Then
      Write(CharBuffer, *) 'Time in Init():                            ',      &
           & InitTF - InitTS, '\n'c
      Call PetscPrintf(PETSC_COMM_SELF, CharBuffer, iErr)
   End If
-#endif
 
 
   Call PetscGetTime(AssembTS, iErr)
 
-  Call Assemb_Mat_Elast(MR, Geom, Params, Elem_db, Node_db)
-
-!  If (MEF90_MyRank == 0) Then
-!     Call MatGetLocalMat(MR, MAT_INITIAL_MATRIX, Loc_Mat, iErr)
-!  End If
-!!$  Print*, 'BC_Ptr: ', Associated(BC_Ptr), Size(BC_Ptr)
-!!$  Allocate(BC_Ptr(Size(Node_db)))
-!!$  Print*, 'BC_Ptr: ', Associated(BC_Ptr), Size(BC_Ptr)
-!!$  BC_Ptr = 0.0_Kr
-!!$  Call Assemb_RHS_Elast(RHS, Geom, Params, Elem_db, Node_db, BC_Dist)
-
-!  Call MEF90_FINALIZE()
-!  STOP
-!  Call MatView(MR, PETSC_VIEWER_DRAW_WORLD, iErr)
-
-#ifdef MEF90_TIMING
+  Call Assemb_Mat_Elast2DA(MR, Geom, Params, Elem_db, Node_db)
+!!!  Call MatView(MR, PETSC_VIEWER_STDOUT_WORLD, iErr)
+!!!
+!!!  Call Finalize()
+!!!  STOP
+  
   Call PetscGetTime(AssembTF, iErr)
   If (MEF90_MyRank == 0) Then
      Write(CharBuffer,*) 'Total time in Assemb_Mat_Elast:        :    ',      &
           & AssembTF - AssembTS, '\n'c
      Call PetscPrintf(PETSC_COMM_WORLD, CharBuffer, iErr)
   End If
-#endif
-
 
   Call Init_KSP()
 
@@ -108,37 +77,28 @@ Program Elast
      !!! EXO -> BC_Ptr -> BC_Master
      If (MEF90_MyRank == 0) Then
         Call VecGetArrayF90(BC_Master, BC_Ptr, iErr)
-        Call Read_EXO_Result_Nodes(Geom, 2, TimeStep, EXO_Ptr, Geom%Num_Dim)
+        Call Read_EXO_Result_Nodes(Geom, 4, TimeStep, EXO_Ptr, 1)
         BC_Ptr = EXO_Ptr
         Call VecRestoreArrayF90(BC_Master, BC_Ptr, iErr)
         DeAllocate(EXO_Ptr)
+
         Call VecGetArrayF90(F_Master, BC_Ptr, iErr)
-        Call Read_EXO_Result_Nodes(Geom, 5, TimeStep, EXO_Ptr, Geom%Num_Dim)
+        Call Read_EXO_Result_Nodes(Geom, 7, TimeStep, EXO_Ptr, 1)
         BC_Ptr = EXO_Ptr
         Call VecRestoreArrayF90(F_Master, BC_Ptr, iErr)
-        DeAllocate(EXO_Ptr)
-        Call VecGetArrayF90(Temp_Master, BC_Ptr, iErr)
-        Call Read_EXO_Result_Nodes(Geom, 8, TimeStep, EXO_Ptr,1)
-        BC_Ptr = EXO_Ptr
-        Call VecRestoreArrayF90(Temp_Master, BC_Ptr, iErr)
         DeAllocate(EXO_Ptr)
      EndIf
 
      !!! XXX_Master -> XXX_Dist
      Call VecScatterBegin(BC_Master, BC_Dist, INSERT_VALUES, SCATTER_REVERSE, &
-          & MySD_Vect%ToMaster, iErr)
+          & MySD%ToMaster, iErr)
      Call VecScatterEnd(BC_Master, BC_Dist, INSERT_VALUES, SCATTER_REVERSE,   &
-          & MySD_Vect%ToMaster, iErr)
+          & MySD%ToMaster, iErr)
 
      Call VecScatterBegin(F_Master, F_Dist, INSERT_VALUES, SCATTER_REVERSE,   &
-          & MySD_Vect%ToMaster, iErr)
+          & MySD%ToMaster, iErr)
      Call VecScatterEnd(F_Master, F_Dist, INSERT_VALUES, SCATTER_REVERSE,     &
-          & MySD_Vect%ToMaster, iErr)
-
-     Call VecScatterBegin(Temp_Master, Temp_Dist, INSERT_VALUES,              &
-     	  & SCATTER_REVERSE, MySD_Scal%ToMaster, iErr)
-     Call VecScatterEnd(Temp_Master, Temp_Dist, INSERT_VALUES,                &
-     	  & SCATTER_REVERSE, MySD_Scal%ToMaster, iErr)
+          & MySD%ToMaster, iErr)
 
      !!! XXX_Dist -> XXX_Loc
      Call VecGhostUpdateBegin(BC_Dist, INSERT_VALUES, SCATTER_FORWARD, iErr)
@@ -147,33 +107,25 @@ Program Elast
      Call VecGhostUpdateBegin(F_Dist, INSERT_VALUES, SCATTER_FORWARD, iErr)
      Call VecGhostUpdateEnd(F_Dist, INSERT_VALUES, SCATTER_FORWARD, iErr)
 
-     Call VecGhostUpdateBegin(Temp_Dist, INSERT_VALUES, SCATTER_FORWARD, iErr)
-     Call VecGhostUpdateEnd(Temp_Dist, INSERT_VALUES, SCATTER_FORWARD, iErr)
-
      !!! BC_Loc -> RHS
      Call PetscGetTime(RHSTS, iErr)
-     Call Assemb_RHS_Elast(RHS, Geom, Params, Elem_db, Node_db, Elem_Scal,    &
-          & Node_Scal, BC_Loc, F_Loc, Temp_Loc)
+     Call Assemb_RHS_Elast2DA(RHS, Geom, Params, Elem_db, Node_db, BC_Loc, F_Loc)
      Call PetscGetTime(RHSTF, iErr)
-     If (MEF90_MyRank == 0) Then
+     If (MEF90_MyRank ==0) Then
         Write(CharBuffer,*) 'Total time in Assemb_RHS_Elast:          ',      &
              & RHSTF - RHSTS, '\n'c
         Call PetscPrintf(PETSC_COMM_SELF, CharBuffer, iErr)
      End If
-!     Call VecView(RHS, PETSC_VIEWER_DRAW_WORLD, iErr)
-
+     
      Call PetscGetTime(SolveTS, iErr)
      Call KSPSolve(KSP_MR, RHS, Sol_Dist, iErr)
 
-!     Call VecView(SOL_Dist, PETSC_VIEWER_DRAW_WORLD, iErr)
-#ifdef MEF90_TIMING
      Call PetscGetTime(SolveTF, iErr)
      If (MEF90_MyRank == 0) Then
         Write(CharBuffer,*) 'Total time in KSP_Solve:                 ',      &
              & SolveTF- SolveTS, '\n'c
         Call PetscPrintf(PETSC_COMM_SELF, CharBuffer, iErr)
      End If
-#endif
 
      If (MEF90_MyRank ==0) Then
         Call KSPGetIterationNumber(KSP_MR, NbIter, iErr)
@@ -182,24 +134,24 @@ Program Elast
         Call PetscPrintf(PETSC_COMM_SELF, CharBuffer, iErr)
      End If
 
+!!! Energy Computation
      Call VecGhostUpdateBegin(SOL_Dist, INSERT_VALUES, SCATTER_FORWARD, iErr)
      Call VecGhostUpdateEnd(SOL_Dist, INSERT_VALUES, SCATTER_FORWARD, iErr)
-     Call Calc_Ener(SOL_Loc, Geom, Params, Elem_db, Node_db, MySD_Vect,       &
-          & F_Loc, Ener_Elast) 
-     Write(CharBuffer, *) '=== Elastic Energy: ', Ener_Elast, '\n'c
+!     Call Calc_Ener(SOL_Loc, Geom, Params, Elem_db, Node_db, MySD,       &
+!          & F_Loc, Ener_Elast) 
+!     Write(CharBuffer, *) '=== Elastic Energy: ', Ener_Elast, '\n'c
      Call PETScPrintf(PETSC_COMM_WORLD, CharBuffer, iErr)
 
+!!! Saving teh results
      Call PetscGetTime(ExportTS, iErr)
      Call Export()
 
-#ifdef MEF90_TIMING
      Call PetscGetTime(ExportTF, iErr)
      If (MEF90_MyRank ==0) Then
         Write(CharBuffer,*) 'Total time in Export:                    ',      &
              & ExportTF - ExportTS, '\n'c
         Call PetscPrintf(PETSC_COMM_SELF, CharBuffer, iErr)
      End If
-#endif
   End Do
 
   Call PetscGetTime(TotalTF, iErr)
@@ -212,5 +164,4 @@ Program Elast
   Call Finalize()
 
   100 Format(A)
-
-End Program Elast
+End Program Elast2DA
--- Elast2DA/m_Elast2DA_Vars.F90
+++ Elast2DA/m_Elast2DA_Vars.F90
@@ -1,8 +1,4 @@
-#ifdef PB_2D
-Module m_Elast2D_Vars
-#else
-Module m_Elast3D_Vars
-#endif
+Module m_Elast2DA_Vars
   Use m_MEF90
   Use m_Rupt_Struct
 
@@ -32,39 +28,20 @@ Module m_Elast3D_Vars
   Vec, Public                                           :: F_Dist, F_Loc
   Vec, Public                                           :: F_Master
 
-  Vec, Public                                           :: Temp_Dist, Temp_Loc
-  Vec, Public                                           :: Temp_Master
-
   KSP, Public                                           :: KSP_MR
   PC, Public                                            :: PC_MR
   KSPConvergedReason, Public                            :: KSP_Reason
 
                                                         
-#ifdef PB_2D
-  Type(Element2D_Elast), Dimension(:), Pointer, Public  :: Elem_db
+  Type(Element2D_Scal), Dimension(:), Pointer, Public   :: Elem_db
   Type(Node2D), Dimension(:), Pointer, Public           :: Node_db
-  Type(Element2D_Scal), Dimension(:), Pointer, Public   :: Elem_Scal
-  Type(Node2D), Dimension(:), Pointer, Public           :: Node_Scal
-
-  Type(MatS2D), Dimension(:), Pointer, Public           :: Stress_Sol
-  Type(MatS2D), Dimension(:), Pointer, Public           :: Strain_Sol
-#else
-  Type(Element3D_Elast), Dimension(:), Pointer, Public  :: Elem_db
-  Type(Node3D), Dimension(:), Pointer, Public           :: Node_db
-  Type(Element3D_Scal), Dimension(:), Pointer, Public   :: Elem_Scal
-  Type(Node3D), Dimension(:), Pointer, Public           :: Node_Scal
-
-  Type(MatS3D), Dimension(:), Pointer, Public           :: Stress_Sol
-  Type(MatS3D), Dimension(:), Pointer, Public           :: Strain_Sol
-#endif
 
 
-  Integer, Public                                       :: GaussOrder=3
+  Integer, Public                                       :: GaussOrder=2
 
   Type(EXO_Geom_Info), Public                           :: Geom
   Type(Rupt_Params), Public                             :: Params
-  Type(SD_Info), Public                                 :: MySD_Vect
-  Type(SD_Info), Public                                 :: MySD_Scal
+  Type(SD_Info), Public                                 :: MySD
 
   Integer, Public                                       :: iErr
   Integer, Public                                       :: TimeStep
@@ -79,14 +56,7 @@ Module m_Elast3D_Vars
   PetscLogDouble, Public                                :: SolveTS, SolveTF
   PetscLogDouble, Public                                :: ExportTS, ExportTF
 
-  Integer, Dimension(:), Pointer, Public                :: NNZ
-!  Integer, Public                                       :: MyIdxMin, MyIdxMax
-
   Real(Kind = Kr), Public                               :: Ener_Elast
   PetscReal, Public, Parameter                          :: VLV = 1.0e+20
 
-#ifdef PB_2D
-End Module m_Elast2D_Vars
-#else
-End Module m_Elast3D_Vars
-#endif
+End Module m_Elast2DA_Vars
