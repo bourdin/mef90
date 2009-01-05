04f4b0685cc2ad2512a75a00abef6b14dd17c9a8
blaise <blaise@macmini.tolula.com>
2009-01-04 21:09:53 -0600

    Partial fix for a nodeset miscounted bug

--- TestSieve/TestSieve.F90
+++ TestSieve/TestSieve.F90
@@ -303,7 +303,7 @@ Program TestSieve
             call MeshGetStratumSize(dMeshTopology%mesh, CharBuffer, setId, dMeshTopology%Node_Set(iSet)%Num_Nodes, ierr)
             Allocate(dMeshTopology%Node_Set(iSet)%Node_ID(dMeshTopology%Node_Set(iSet)%Num_Nodes))
             call MeshGetStratum(dMeshTopology%mesh, CharBuffer, setId, dMeshTopology%Node_Set(iSet)%Node_ID, ierr)
-            dMeshTopology%Node_Set(iSet)%Node_ID = dMeshTopology%Node_Set(iSet)%Node_ID - dMeshTopology%Num_Elems
+            dMeshTopology%Node_Set(iSet)%Node_ID = dMeshTopology%Node_Set(iSet)%Node_ID - dMeshTopology%Num_Elems + 1
          End Do
       End If
       Deallocate(setIds)
