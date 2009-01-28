9c67d3232bc141321d4703d816333659f0b82692
Blaise Bourdin <bourdin@lsu.edu>
2009-01-28 19:49:11 +0100

    messing around with assemblyMatrix

--- TestSieve/TestAssembly.F90
+++ TestSieve/TestAssembly.F90
@@ -26,10 +26,10 @@ Program TestAssembly
    Character(len=256)                           :: CharBuffer, IOBuffer, filename
    Character(len=256)                           :: prefix
    Type(PetscViewer)                            :: viewer, myviewer
-   Type(SectionReal)                            :: U_Sec
+   Type(SectionReal)                            :: U_Sec, DefaultSection
    Type(Vec)                                    :: U_VecG, U_VecL
    PetscReal, Dimension(:), Pointer             :: val
-   PetscReal, DImension(:,:), Pointer           :: Kelem
+   PetscReal, Dimension(:), Pointer             :: Kelem
    Type(Mat)                                    :: K
    Type(VecScatter)                             :: scatter
    PetscInt                                     :: exo_ver
@@ -52,7 +52,7 @@ Program TestAssembly
    EXO%filename = Trim(prefix)//'.gen'
 
 
-   Call MeshTopologyReadEXO(MeshTopology, Coords, Elem2DA, EXO)
+   Call MeshTopologyReadEXO(MeshTopology, EXO)
    
    MeshTopology%Elem_Blk%Elem_Type    = MEF90_P1_Lagrange
    Do iBlk = 1, MeshTopology%Num_Elem_Blks
@@ -78,25 +78,30 @@ Program TestAssembly
    Call VecSet(U_VecG, 1.0_Kr, iErr); CHKERRQ(iErr)
    Call VecView(U_VecG, PetscViewer(PETSC_VIEWER_STDOUT_WORLD), iErr); CHKERRQ(iErr)
    
+   i = 1
+   Call MeshSetMaxDof(MeshTopology%Mesh, i, iErr); CHKERRQ(iErr) 
 
+
+   Call MeshGetSectionReal(MeshTopology%mesh, 'default', DefaultSection, iErr); CHKERRQ(ierr)
    Allocate(val(3))
-   Allocate(Kelem(3,3))
+   Allocate(Kelem(9))
    val = 1.0_Kr
    Kelem = 1.0_Kr
-   Do i = 1, MeshTopology%Num_Elems
+   Do i = 0, MeshTopology%Num_Elems-1
 !      Call assemblevector(U_VecG, i, val, ADD_VALUES, iErr); CHKERRQ(iErr)
-      Call assembleMatrix(K, MeshTopology%mesh, U_Sec, i-1, Kelem, ADD_VALUES, iErr); CHKERRQ(iErr)
+      Call assembleMatrix(K, MeshTopology%mesh, DefaultSection, i, Kelem, ADD_VALUES, iErr); CHKERRQ(iErr)
    End Do
    DeAllocate(val)
    DeAllocate(Kelem)
-   Call VecView(U_VecG, PetscViewer(PETSC_VIEWER_STDOUT_WORLD), iErr); CHKERRQ(iErr)
-
+!   Call VecView(U_VecG, PetscViewer(PETSC_VIEWER_STDOUT_WORLD), iErr); CHKERRQ(iErr)
+   Call MatAssemblyBegin(K, MAT_FINAL_ASSEMBLY, iErr); CHKERRQ(iErr)
+   Call MatAssemblyEnd(K, MAT_FINAL_ASSEMBLY, iErr); CHKERRQ(iErr)
+   Call MatView(K, PETSC_VIEWER_STDOUT_WORLD, iErr); CHKERRQ(iErr)
    Call VecDestroy(U_VecG, iErr); CHKERRQ(iErr)
    Call VecDestroy(U_VecL, iErr); CHKERRQ(iErr)
 
    Call MeshTopologyDestroy(MeshTopology)
    Call SectionRealDestroy(U_Sec, iErr); CHKERRQ(iErr)
-   DeAllocate(V2D)
    
    
    If (verbose) Then
