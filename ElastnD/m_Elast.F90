53bdda6818fac3b5982020a8fe1a8fb2254f4806
Blaise Bourdin <bourdin@lsu.edu>
2009-04-16 13:08:16 -0500

    commit after merge

--- ElastnD/m_Elast.F90
+++ ElastnD/m_Elast.F90
@@ -1,5 +1,5 @@
 #if defined PB_2D
-Module m_2D
+Module m_Elast2D
 #elif defined PB_3D
 Module m_Elast3D
 #endif
@@ -51,10 +51,12 @@ Module m_Elast3D
       Type(Element3D_Elast), Dimension(:), Pointer  :: ElemU
 #endif
       Type(SectionReal)                            :: U
-      Type(SectionReal)                            :: Stress
-      Type(SectionReal)                            :: Strain
+      Type(SectionReal)                            :: StressU
+      Type(SectionReal)                            :: StrainU
       Type(SectionReal)                            :: F
       Type(SectionReal)                            :: Theta
+      PetscReal                                    :: Load
+      PetscInt                                     :: TimeStep
       PetscReal                                    :: BulkEnergy
       Type(VecScatter)                             :: ScatterU
       Type(SectionInt)                             :: BCFlagU
@@ -69,21 +71,141 @@ Module m_Elast3D
       Type(MatProp3D_Type)                         :: MatProp
 #endif
       Type(AppParam_Type)                          :: AppParam
-      Type(RuptSchemeParam_Type)                   :; RuptSchemeParam
+      Type(RuptSchemeParam_Type)                   :: RuptSchemeParam
    End Type AppCtx_Type
    
    
 Contains
 
    Subroutine ElastInit(AppCtx)
+      Type(AppCtx_Type)                            :: AppCtx
+
+      PetscInt                                     :: iErr, i
+      Type(Mesh)                                   :: Tmp_Mesh
+      Character(len=MEF90_MXSTRLEN)                :: IOBuffer, filename
+      PetscInt                                     :: NumComponents
+      PetscTruth                                   :: HasPrefix
+
+      
+      Call MEF90_Initialize()
+      Call PetscOptionsHasName(PETSC_NULL_CHARACTER, '-verbose', AppCtx%AppParam%Verbose, iErr)    
+      Call PetscOptionsGetString(PETSC_NULL_CHARACTER, '-p',     AppCtx%AppParam%prefix, HasPrefix, iErr); CHKERRQ(iErr)
+      If (.NOT. HasPrefix) Then
+         Call PetscPrintf(PETSC_COMM_WORLD, "No input file prefix given\n", iErr)
+         Call MEF90_Finalize()
+         STOP
+      End If
+      
+      If (AppCtx%AppParam%verbose) Then
+         Write(filename, 101) Trim(AppCtx%AppParam%prefix), MEF90_MyRank
+         Call PetscViewerASCIIOpen(PETSC_COMM_SELF, filename, AppCtx%AppParam%MyLogViewer, iErr); CHKERRQ(iErr);   
+         Write(IOBuffer, 102) MEF90_MyRank, Trim(filename)
+         Call PetscSynchronizedPrintf(PETSC_COMM_WORLD, IOBuffer, iErr); CHKERRQ(iErr)
+         Call PetscSynchronizedFlush (PETSC_COMM_WORLD, iErr); CHKERRQ(iErr)
+   
+         Write(filename, 103) Trim(AppCtx%AppParam%prefix)
+         Call PetscViewerASCIIOpen(PETSC_COMM_WORLD, filename, AppCtx%AppParam%LogViewer, iErr); CHKERRQ(iErr);   
+         Write(IOBuffer, 104) Trim(filename)
+         Call PetscPrintf(PETSC_COMM_WORLD, IOBuffer, iErr); CHKERRQ(iErr)
+      End If
+   
+101 Format(A, '-', I4.4, '.log')
+102 Format('Output from processor ', I4.4, ' redirected to file ', A, '\n'c)
+103 Format(A,'.log')
+104 Format('Collective output redirected to file ', A, '\n'c)
+
+      Call Write_EXO_Case(AppCtx%AppParam%prefix, '%0.4d', MEF90_NumProcs)
+      AppCtx%EXO%Comm = PETSC_COMM_WORLD
+      AppCtx%EXO%filename = Trim(AppCtx%AppParam%prefix)//'.gen'
+
+         !!! Reading and distributing sequential mesh
+      If (MEF90_NumProcs == 1) Then
+         Call MeshCreateExodus(PETSC_COMM_WORLD, AppCtx%EXO%filename, AppCtx%MeshTopology%mesh, ierr); CHKERRQ(iErr)
+      Else
+         Call MeshCreateExodus(PETSC_COMM_WORLD, AppCtx%EXO%filename, Tmp_mesh, ierr); CHKERRQ(iErr)
+         Call MeshDistribute(Tmp_mesh, PETSC_NULL_CHARACTER, AppCtx%MeshTopology%mesh, ierr); CHKERRQ(iErr)
+         Call MeshDestroy(Tmp_mesh, ierr); CHKERRQ(iErr)
+      End If
+   
+      Call MeshTopologyReadEXO(AppCtx%MeshTopology, AppCtx%EXO)
+      If (AppCtx%AppParam%verbose) Then
+         Write(IOBuffer, *) "Done reading and partitioning the mesh\n"c
+         Call PetscPrintf(PETSC_COMM_WORLD, IOBuffer, iErr); CHKERRQ(iErr)
+      End If
+   
+      AppCtx%MyEXO%comm = PETSC_COMM_SELF
+      AppCtx%MyEXO%exoid = AppCtx%EXO%exoid
+      Write(AppCtx%MyEXO%filename, 99) trim(AppCtx%AppParam%prefix), MEF90_MyRank
+ 99  Format(A, '-', I4.4, '.gen')
+   
+      !!! Initializes the values and names of the properties and variables
+      Call RuptEXOVariable_Init(AppCtx%MyEXO)
+      Call RuptEXOProperty_Init(AppCtx%MyEXO, AppCtx%MeshTopology)   
+      If (AppCtx%AppParam%verbose) Then
+         Write(IOBuffer, *) "Done with RuptEXOVariable_Init and RuptEXOProperty_Init\n"c
+         Call PetscPrintf(PETSC_COMM_WORLD, IOBuffer, iErr); CHKERRQ(iErr)
+      End If
+
+      !!! Set the element type for each block so that we can call ElementInit
+      Do i = 1, AppCtx%MeshTopology%num_elem_blks
+         AppCtx%MeshTopology%elem_blk(i)%Elem_Type = AppCtx%MyEXO%EBProperty( Rupt_EBProp_Elem_Type )%Value( AppCtx%MeshTopology%elem_blk(i)%ID )
+         Call Init_Elem_Blk_Type(AppCtx%MeshTopology%Elem_Blk(i), AppCtx%MeshTopology%num_dim)
+      End Do
+      Call ElementInit(AppCtx%MeshTopology, AppCtx%ElemU, AppCtx%RuptSchemeParam%IntegOrder)
+      If (AppCtx%AppParam%verbose) Then
+         Write(IOBuffer, *) "Done with ElementInit\n"c
+         Call PetscPrintf(PETSC_COMM_WORLD, IOBuffer, iErr); CHKERRQ(iErr)
+      End If
+
+      !!! Create the Sections for the variables
+      Call MeshGetVertexSectionReal(AppCtx%MeshTopology%mesh, AppCtx%MeshTopology%Num_Dim, AppCtx%U, iErr); CHKERRQ(iErr)
+      Call MeshGetVertexSectionReal(AppCtx%MeshTopology%mesh, AppCtx%MeshTopology%Num_Dim, AppCtx%F, iErr); CHKERRQ(iErr)
+      Call MeshGetVertexSectionReal(AppCtx%MeshTopology%mesh, 1, AppCtx%Theta, iErr); CHKERRQ(iErr)
+
+      NumComponents = AppCtx%MeshTopology%Num_Dim * (AppCtx%MeshTopology%Num_Dim + 1) / 2
+      Call MeshGetVertexSectionReal(AppCtx%MeshTopology%mesh, NumComponents, AppCtx%StrainU, iErr); CHKERRQ(iErr)
+      Call MeshGetVertexSectionReal(AppCtx%MeshTopology%mesh, NumComponents, AppCtx%StressU, iErr); CHKERRQ(iErr)
+
+      !!! Create the Scatter, Vec, Mat, KSP, PC
+      Call MeshCreateGlobalScatter(AppCtx%MeshTopology%mesh, AppCtx%U, AppCtx%ScatterU, iErr); CHKERRQ(iErr)
+      Call MeshCreateVector(AppCtx%MeshTopology%mesh, AppCtx%U, AppCtx%RHSU, iErr); CHKERRQ(iErr)
+
+      Call MeshSetMaxDof(AppCtx%MeshTopology%Mesh, 1, iErr); CHKERRQ(iErr) 
+      Call MeshCreateMatrix(AppCtx%MeshTopology%mesh, AppCtx%U, MATMPIAIJ, AppCtx%KU, iErr); CHKERRQ(iErr)
+      
+      Call KSPCreate(PETSC_COMM_WORLD, AppCtx%KSPU, iErr); CHKERRQ(iErr)
+      Call KSPSetOperators(AppCtx%KSPU, AppCtx%KU, AppCtx%KU, SAME_NONZERO_PATTERN, iErr); CHKERRQ(iErr)
+      Call KSPSetType(AppCtx%KSPU, KSPCG, iErr); CHKERRQ(iErr)
+      Call KSPSetInitialGuessNonzero(AppCtx%KSPU, PETSC_TRUE, iErr); CHKERRQ(iErr)
+      Call KSPSetFromOptions(AppCtx%KSPU, iErr); CHKERRQ(iErr)
+
+      Call KSPGetPC(AppCtx%KSPU, AppCtx%PCU, iErr); CHKERRQ(iErr)
+      Call PCSetType(AppCtx%PCU, PCBJACOBI, iErr); CHKERRQ(iErr)
+
+      !!! Create the Section for the BC
+      Call MeshGetVertexSectionInt(AppCtx%MeshTopology%mesh, AppCtx%MeshTopology%Num_Dim, AppCtx%BCFlagU, iErr); CHKERRQ(iErr)
+#if defined PB_2D
+      Call EXOProperty_InitBCFlag2D(AppCtx%EXO, AppCtx%MeshTopology, AppCtx%BCFlagU)
+#elif defined PB_3D
+      Call EXOProperty_InitBCFlag3D(AppCtx%EXO, AppCtx%MeshTopology, AppCtx%BCFlagU)
+#endif
+
+      !!! Read U, F, and Temperature
+      Call Read_EXO_Result_Vertex(AppCtx%MyEXO, AppCtx%MeshTopology, AppCtx%MyEXO%VertVariable(Rupt_VertVar_DisplacementX)%Offset, AppCtx%TimeStep, AppCtx%U) 
+      Call Read_EXO_Result_Cell(AppCtx%MyEXO, AppCtx%MeshTopology, AppCtx%MyEXO%VertVariable(Rupt_CellVar_StrainXX)%Offset, AppCtx%TimeStep, AppCtx%StrainU) 
+      Call Read_EXO_Result_Cell(AppCtx%MyEXO, AppCtx%MeshTopology, AppCtx%MyEXO%VertVariable(Rupt_CellVar_StressXX)%Offset, AppCtx%TimeStep, AppCtx%StressU) 
+
+
+      AppCtx%TimeStep = 1
       !!! BB
    End Subroutine ElastInit
 !----------------------------------------------------------------------------------------!      
 ! Solve (CM)   
 ! No changes wrt VectPoisson
 !----------------------------------------------------------------------------------------!      
-
+   
    Subroutine Solve(AppCtx)
+      !!! CM
       Type(AppCtx_Type)                            :: AppCtx
       
       PetscInt                                     :: iErr
@@ -113,8 +235,9 @@ Contains
 !----------------------------------------------------------------------------------------!      
 ! MatAssembly (CM)  
 !----------------------------------------------------------------------------------------!      
-       
+   
    Subroutine MatAssembly(AppCtx)
+      !!! CM
       Type(AppCtx_Type)                            :: AppCtx
       
       PetscInt                                     :: iBlk, iE, iELoc, iErr
@@ -146,8 +269,15 @@ Contains
 !----------------------------------------------------------------------------------------!      
    
    Subroutine MatAssemblyLocal(iE, Mat_Prop, AppCtx, MatElem)
+      !!! CM
+      PetscInt                                     :: iE
+#if defined PB_2D
+      Type(MatProp2D_Type)                         :: Mat_Prop
+#elif
+      Type(MatProp3D_Type)                         :: Mat_Prop
+#endif
       Type(AppCtx_Type)                            :: AppCtx
-      PetscReal, Dimension(:,:), Pointer           :: MatElem 
+      PetscReal, Dimension(:,:), Pointer           :: MatElem      
       PetscInt                                     :: iE
    
       PetscInt                                     :: iErr
@@ -178,12 +308,13 @@ Contains
       
       DeAllocate(BCFlag)
       Call PetscLogEventEnd(AppCtx%LogInfo%MatAssemblyLocal_Event, iErr); CHKERRQ(iErr)
-   End Subroutine MatAssemblyLocal  
-   
+   End Subroutine MatAssemblyLocal
+
 !----------------------------------------------------------------------------------------!      
 ! RHSAssembly (CM)  
 !----------------------------------------------------------------------------------------!      
    Subroutine RHSAssembly(AppCtx)
+      !!! CM
       Type(AppCtx_Type)                            :: AppCtx
       
       PetscInt                                     :: iErr
@@ -217,6 +348,7 @@ Contains
 ! RHSAssemblyLocal (CM)  
 !----------------------------------------------------------------------------------------!      
    Subroutine RHSAssemblyLocal(iE, Mat_Prop, AppCtx, RHSElem)
+      !!! CM
       PetscInt                                     :: iE
       Type(AppCtx_Type)                            :: AppCtx
       PetscReal, Dimension(:), Pointer             :: RHSElem
@@ -229,7 +361,7 @@ Contains
 	  Type (Vect2D)             				   :: TmpRHS
 #elif defined PB_3D  
 	  Type (Vect3D)             				   :: TmpRHS    
-#endif      
+#endif
       RHSElem  = 0.0_Kr
       NumDoF   = Size(AppCtx%Elem(iE)%BF,1)
       NumGauss = Size(AppCtx%Elem(iE)%BF,2)
@@ -254,11 +386,12 @@ Contains
       DeAllocate(F)
       Call PetscLogEventEnd(AppCtx%LogInfo%RHSAssemblyLocal_Event, iErr); CHKERRQ(iErr)
    End Subroutine RHSAssemblyLocal
-
+   
 !----------------------------------------------------------------------------------------!      
 ! ComputeEnergy (CM)  
 !----------------------------------------------------------------------------------------!      
    Subroutine ComputeEnergy(AppCtx)
+      !!! CM
       Type(AppCtx_Type)                            :: AppCtx
       
       PetscInt                                     :: iErr
@@ -367,12 +500,44 @@ Contains
 !----------------------------------------------------------------------------------------!      
    
    Subroutine Save(AppCtx)
-      !!! BB
+      Type(AppCtx_Type)                            :: AppCtx
+
+      Call Write_EXO_Result_Vertex(AppCtx%MyEXO, AppCtx%MeshTopology, AppCtx%MyEXO%VertVariable(Rupt_VertVar_DisplacementX)%Offset, AppCtx%TimeStep, AppCtx%U) 
+      Call Write_EXO_Result_Cell(AppCtx%MyEXO, AppCtx%MeshTopology, AppCtx%MyEXO%VertVariable(Rupt_CellVar_StrainXX)%Offset, AppCtx%TimeStep, AppCtx%StrainU) 
+      Call Write_EXO_Result_Cell(AppCtx%MyEXO, AppCtx%MeshTopology, AppCtx%MyEXO%VertVariable(Rupt_CellVar_StressXX)%Offset, AppCtx%TimeStep, AppCtx%StressU) 
+      Call Write_EXO_Result_Global(AppCtx%MyEXO, AppCtx%MyEXO%GlobVariable(Rupt_GlobVar_Load)%Offset, AppCtx%TimeStep, AppCtx%BulkEnergy)
    End Subroutine Save
    
    Subroutine ElastFinalize(AppCtx)
-      !!! BB
-   End Subroutine ELastFinalize
+      Type(AppCtx_Type)                            :: AppCtx
+
+      PetscInt                                     :: iErr
+      Character(len=MEF90_MXSTRLEN)                :: filename
+   
+      Call SectionRealDestroy(AppCtx%U, iErr); CHKERRQ(iErr)
+      Call SectionRealDestroy(AppCtx%F, iErr); CHKERRQ(iErr)
+      Call SectionRealDestroy(AppCtx%Theta, iErr); CHKERRQ(iErr)
+      Call SectionRealDestroy(AppCtx%StrainU, iErr); CHKERRQ(iErr)
+      Call SectionRealDestroy(AppCtx%STressU, iErr); CHKERRQ(iErr)
+      
+      Call VecScatterDestroy(AppCtx%ScatterU, iErr); CHKERRQ(iErr)
+      Call SectionIntDestroy(AppCtx%BCFlagU, iErr); CHKERRQ(iErr)
+      Call MatDestroy(AppCtx%KU, iErr); CHKERRQ(iErr)
+      Call VecDestroy(AppCtx%RHSU, iErr); CHKERRQ(iErr)
+      Call KSPDestroy(AppCtx%KSPU, iErr); CHKERRQ(iErr)
+      Call MeshDestroy(AppCtx%MeshTopology%Mesh, iErr); CHKERRQ(ierr)
+
+      If (AppCtx%AppParam%verbose) Then
+         Call PetscViewerFlush(AppCtx%AppParam%MyLogViewer, iErr); CHKERRQ(iErr)
+         Call PetscViewerFlush(AppCtx%AppParam%LogViewer, iErr); CHKERRQ(iErr)
+         Call PetscViewerDestroy(AppCtx%AppParam%MyLogViewer, iErr); CHKERRQ(iErr)
+         Call PetscViewerDestroy(AppCtx%AppParam%LogViewer, iErr); CHKERRQ(iErr)
+      End If
+      Write(filename, 103) Trim(AppCtx%AppParam%prefix)
+      Call PetscLogPrintSummary(PETSC_COMM_WORLD, filename, iErr); CHKERRQ(iErr)
+      Call MEF90_Finalize()
+103 Format(A,'.log')
+   End Subroutine ElastFinalize
    
    
 #if defined PB_2D
