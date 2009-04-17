c6561c7170eeacb5b46eb6f03adb71810ff0b621
Blaise Bourdin <bourdin@lsu.edu>
2009-04-17 17:24:08 -0500

    found more unmatched Allocate

--- ElastnD/Elast.F90
+++ ElastnD/Elast.F90
@@ -50,7 +50,8 @@ Program  Elast
    End If
    
    Call RHSAssembly(AppCtx)
-   Call VecView(APpCtx%RHSU, PETSC_VIEWER_STDOUT_WORLD, iErr); CHKERRQ(iErr)
+   Call VecView(AppCtx%RHSU, PETSC_VIEWER_STDOUT_WORLD, iErr); CHKERRQ(iErr)
+   
    If (AppCtx%AppParam%verbose) Then
       Write(IOBuffer, *) 'Calling KSPSolve\n'c
       Call PetscPrintf(PETSC_COMM_WORLD, IOBuffer, iErr); CHKERRQ(iErr)
