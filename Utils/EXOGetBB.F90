923b1a2e9a9d98beb5d42be67cd7a2b299eff15b
Blaise Bourdin <bourdin@math.lsu.edu>
2009-11-11 11:59:48 +0100

    Moved Partitioner and added EXOGetBB

---
+++ Utils/EXOGetBB.F90
@@ -0,0 +1,47 @@
+Program EXOGetBB
+
+#include "finclude/petscdef.h"
+#include "finclude/petscmeshdef.h"
+
+   Use m_MEF90
+   Use petsc
+   Use petscmesh
+
+   Implicit NONE   
+
+   Type(MeshTopology_Type)                      :: MeshTopology
+   Type(EXO_Type)                               :: EXO, MyEXO
+   Type(Vect2D), Dimension(:), Pointer          :: CoordVect2D
+   Type(Vect3D), Dimension(:), Pointer          :: CoordVect3D
+   
+   PetscTruth                                   :: HasPrefix
+   PetscReal, Dimension(:,:), Pointer           :: CoordArray
+   PetscErrorCode                               :: iErr
+   Character(len=256)                           :: prefix
+   
+   Call MEF90_Initialize()
+   Call PetscOptionsGetString(PETSC_NULL_CHARACTER, '-p', prefix, HasPrefix, iErr)    
+   If (.NOT. HasPrefix) Then
+      Call PetscPrintf(PETSC_COMM_WORLD, "No input file prefix given\n", iErr)
+      Call MEF90_Finalize()
+      STOP
+   End If
+
+   EXO%Comm = PETSC_COMM_WORLD
+   EXO%filename = Trim(prefix)//'.gen'
+
+
+   Call MeshCreateExodus(PETSC_COMM_WORLD, EXO%filename, MeshTopology%mesh, ierr); CHKERRQ(iErr)
+
+   Call MeshGetCoordinatesF90(MeshTopology%mesh, CoordArray, iErr); CHKERRQ(iErr)
+   Write(*, *) 'Size(CoordArray)', Size(CoordArray,1), Size(CoordArray,2)
+   Write(*,*) 'X range: ', MinVal(CoordArray(:,1)), MaxVal(CoordArray(:,1))
+   Write(*,*) 'Z range: ', MinVal(CoordArray(:,2)), MaxVal(CoordArray(:,2))
+   If (MeshTopology%Num_Dim ==3) Then
+      Write(*,*) 'Z range: ', MinVal(CoordArray(:,3)), MaxVal(CoordArray(:,3))
+   End If
+   Call MeshRestoreCoordinatesF90(MeshTopology%mesh, CoordArray, iErr); CHKERRQ(iErr)
+
+   Call MeshDestroy(MeshTopology%Mesh)
+   Call MEF90_Finalize()
+End Program EXOGetBB
