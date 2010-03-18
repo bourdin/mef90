7a09540a5ddc8d44701bf04770b2e3982a6022d2
Blaise Bourdin <bourdin@math.lsu.edu>
2010-03-17 18:19:31 -0700

    minor python work

--- python/pymef90/energies.py
+++ python/pymef90/energies.py
@@ -10,13 +10,13 @@ def plot(energies, showwork=False):
   plt.xlabel('t')
   plt.ylabel('Energy')
   plt.title('Energies vs normalized time')
-  
+  ###
   return 0
 
 def getlaststep(fname):
-### open file
+  ### open file
   f=open(fname)
-### Read last line in a string
+  ### Read last line in a string
   lastline = f.readlines()[-1]
   laststep = lastline.rsplit()[0] 
   return(int(laststep))
@@ -27,13 +27,21 @@ def save(fname, energies):
 
 def fixBT(energies):
   import numpy as np
-  
+  ###
   laststep = energies[-1,0]
   maxstep  = energies[:,0].max()
-  
+  ###
   energiesBT = np.zeros([laststep,energies.shape[1]])
   for i in range(energies.shape[0]):
     step = energies[i,0]
     if step <= laststep:
       energiesBT[step-1,:] = energies[i,:]
+    ###
   return energiesBT
+
+def computeG(t, Eel, l):
+  import numpy as np
+  ###
+  G = -(Eel[1:]/pow(t[1:],2)-Eel[:-1]/np.power(t[:-1],2)) / (l[1:]-l[:-1])
+  ###
+  return G
