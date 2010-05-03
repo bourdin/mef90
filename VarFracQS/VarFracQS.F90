22ba442d047d3ff024fa5d6c9bde17bebe3dc986
Blaise Bourdin <bourdin@lsu.edu>
2010-05-03 15:17:46 -0700

    Small bug where empty lines were printed on stdout instead of the std out

--- VarFracQS/VarFracQS.F90
+++ VarFracQS/VarFracQS.F90
@@ -202,8 +202,16 @@ Program  VarFracQS
             AppCtx%IsBT = PETSC_TRUE
             AppCtx%TimeStep = max(1, iBTStep-1)
             !!! Insert 2 blank lines in the energy file so that gnuplot breaks lines
-            Write(AppCtx%AppParam%Ener_Unit, *)
-            Write(AppCtx%AppParam%Ener_Unit, *)
+            If (MEF90_MyRank ==0) Then
+               !!! Insert 2 blank lines in the energy files so that gnuplot breaks lines
+               Write(AppCtx%AppParam%Ener_Unit, *)
+               Write(AppCtx%AppParam%Ener_Unit, *)
+            
+               Do iBlk = 1, AppCtx%MeshTopology%Num_Elem_Blks_Global
+                  Write(AppCtx%AppParam%EnerBlock_Unit(iBlk), *)
+                  Write(AppCtx%AppParam%EnerBlock_Unit(iBlk), *)
+               End Do
+            End If
          Else
             AppCtx%IsBT = PETSC_FALSE
          End If
