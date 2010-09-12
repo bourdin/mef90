43e26676bc40a31d3a2fb3d194449632d4c85590
Corrado Maurini <cmaurini@gmail.com>
2010-09-12 18:05:10 +0200

    Add a visit python script: Visit_FracturePNG2.py

---
+++ python/Visit_FracturePNG2.py
@@ -0,0 +1,93 @@
+#        Visit Script to generate and save PNG images of a mef90-sieve output
+#
+#                  How to use
+#
+# 1) set the environmental variable FILE_PREFIX to set the file name:
+#    e.g., for the file set ShrinkingSlab2D-X1-Y2-nc1-lc0.05-h0.025-XXXX.gen
+#    "export FILE_PREFIX=ShrinkingSlab2D-X1-Y2-nc1-lc0.05-h0.025"
+#    
+# 1) run VisIt IN THE DIRECTORY WHERE THE FILE IS LOCATED with Command Line Interface and No Windows:
+#    "visit -cli -nowin -s Visit_FracturePNG.py"
+#
+# Note: the script identify the database name from the .e2c filename 
+# To do: consider the case qith more than 1 .e2c file
+#
+import os
+import sys
+import glob
+# --------------------------------------------------
+# Step 0; Files and directories
+# --------------------------------------------------
+## Get the working directory
+#Directory=os.environ.get('WORKDIR')+'/'
+Directory=os.environ.get('PWD') + '/'
+## Get the prefix for the file name
+#############
+Prefix=glob.glob('*.e2c')[0].split('.e2c')[0]
+#Prefix=os.environ.get('GENPREFIX')
+## Create directory to store images
+#ImageDir=Directory+'Images_'+Prefix+'/'
+#if not os.path.isdir(ImageDir):
+#  os.mkdir(ImageDir)
+##  
+## Step 1: Open the database
+##
+MyDatabase= Prefix+'-*.gen database'
+OpenDatabase(MyDatabase,0)
+# --------------------------------------------------
+# Step 2: Add plots and set their properties
+# --------------------------------------------------
+AddPlot('Pseudocolor', 'Fracture')
+#AddPlot('Mesh','mesh')
+p = PseudocolorAttributes()
+# Set the min/max values
+p.minFlag = 1
+p.maxFlag = 1
+p.min=0.0
+p.max=1.0
+p.legendFlag=0
+#p.centering='Nodal'
+#p.colorTableName='hot'
+SetPlotOptions(p)
+# Step 3: Draw the plots
+DrawPlots()
+# Set the view
+#v = GetView2D()
+#windowCoords = (-0.776642, 1.70354, -0.425681, 0.626574)
+#viewportCoords = (0, 2, 0, 1)
+#SetView2D(v)
+AnnotationAtts = AnnotationAttributes()
+AnnotationAtts.axes2D.visible = 0
+AnnotationAtts.userInfoFlag = 0
+AnnotationAtts.databaseInfoFlag = 0
+AnnotationAtts.legendInfoFlag = 0
+AnnotationAtts.axesArray.visible = 0
+SetAnnotationAttributes(AnnotationAtts)
+# --------------------------------------------------
+# Step 3: Set Save file parameters 
+# --------------------------------------------------
+s = SaveWindowAttributes()
+s.format = s.PNG
+s.saveTiled = 0
+s.family=0
+s.width = 600
+s.height = 200
+s.screenCapture = 0
+s.progressive = 1
+forceMerge = 1
+s.quality = 80
+#
+# Save images of all time steps and add each image filename to a list.
+#
+names = []
+for state in range(1,TimeSliderGetNStates()):
+   SetTimeSliderState(state)
+   # Save the image
+   s.fileName = Directory+ Prefix+'-%04i'%state
+   SetSaveWindowAttributes(s)
+   n = SaveWindow()
+   names = names + [n]
+   print "Processing image for frame %s" %state
+   #print "Processing image for frame %s" %n
+#print names
+exit()
