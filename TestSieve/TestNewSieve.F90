5597fd46d3e9f9b18eee193bb97e00b8aeff2675
Blaise Bourdin <bourdin@math.lsu.edu>
2011-04-26 09:47:39 -0500

    Updated TestNewSieve

--- TestSieve/TestNewSieve.F90
+++ TestSieve/TestNewSieve.F90
@@ -7,12 +7,16 @@ Program TestNewSieve
    Type(DM)                                     :: mesh
    Character(len=256)                           :: IOBuffer, filename
    PetscBool                                    :: flg
-	PetscErrorCode                               :: ierr
+   PetscErrorCode                               :: ierr
+   Type(SectionReal)                            :: S
    
    Call PetscInitialize(PETSC_NULL_CHARACTER,ierr);CHKERRQ(ierr)
 	Call PetscOptionsGetString(PETSC_NULL_CHARACTER,'-f',filename,flg,ierr);CHKERRQ(ierr)
    Call DMMeshCreateExodus(PETSC_COMM_WORLD,filename,mesh,ierr);CHKERRQ(ierr)
    Call DMView(mesh,PETSC_VIEWER_STDOUT_WORLD,ierr);CHKERRQ(ierr)
+
+   Call DMMeshGetVertexSectionReal(mesh,'U',1,S,iErr);CHKERRQ(iErr)
+   Call SectionRealDestroy(S,ierr);CHKERRQ(ierr)
    Call DMDestroy(mesh,ierr);CHKERRQ(ierr)
    Call PetscFinalize(ierr)
 End Program TestNewSieve
