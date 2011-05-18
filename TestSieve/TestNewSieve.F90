81825136697c7cb4c56ea089a491c36db3aa46e7
Blaise Bourdin <bourdin@lsu.edu>
2011-05-17 20:50:12 -0500

    messing around with Mesh issues

--- TestSieve/TestNewSieve.F90
+++ TestSieve/TestNewSieve.F90
@@ -1,34 +1,46 @@
 Program TestNewSieve
 #include "finclude/petscdef.h"
 
-   Use petsc
-   Implicit NONE
-   
-   Type(DM)                                     :: mesh
-   Character(len=256)                           :: IOBuffer, filename
-   PetscBool                                    :: flg
-   PetscErrorCode                               :: ierr
-   Type(SectionReal)                            :: S
-   PetscReal, Dimension(:,:), Pointer           :: Coord
-   PetscInt, Dimension(:,:), Pointer            :: connect
-   
-   Call PetscInitialize(PETSC_NULL_CHARACTER,ierr);CHKERRQ(ierr)
-	Call PetscOptionsGetString(PETSC_NULL_CHARACTER,'-f',filename,flg,ierr);CHKERRQ(ierr)
-   Call DMMeshCreateExodus(PETSC_COMM_WORLD,filename,mesh,ierr);CHKERRQ(ierr)
-   Call DMView(mesh,PETSC_VIEWER_STDOUT_WORLD,ierr);CHKERRQ(ierr)
+	Use petsc
+	Implicit NONE
+	
+	Type(DM)                                     :: mesh
+	Character(len=256)                           :: IOBuffer,infile,outfile
+	Type(PetscViewer)                            :: meshViewer
+	PetscBool                                    :: flg
+	PetscErrorCode                               :: ierr
+	PetscReal, Dimension(:,:), Pointer           :: Coord
+	PetscInt, Dimension(:,:), Pointer            :: connect
+	
+	Call PetscInitialize(PETSC_NULL_CHARACTER,ierr);CHKERRQ(ierr)
 
-   Call DMMeshGetVertexSectionReal(mesh,'U',1,S,iErr);CHKERRQ(iErr)
-   Call SectionRealDestroy(S,ierr);CHKERRQ(ierr)
-   
-   Call DMMeshGetCoordinatesF90(mesh,Coord,ierr);CHKERRQ(ierr)
-   Write(*,*) 'Coordinates: '
-   Write(*,*) Coord
-   Call DMMeshRestoreCoordinatesF90(mesh,Coord,ierr);CHKERRQ(ierr)
-   
-   Call DMMeshGetElementsF90(mesh,connect,ierr);CHKERRQ(ierr)
-   write(*,*) 'Connect: '
-   write(*,*) connect
-   Call DMMeshRestoreElementsF90(mesh,connect,ierr);CHKERRQ(ierr)
-   Call DMDestroy(mesh,ierr);CHKERRQ(ierr)
-   Call PetscFinalize(ierr)
+	Call PetscOptionsGetString(PETSC_NULL_CHARACTER,'-i',infile,flg,ierr);CHKERRQ(ierr)
+	 	
+#ifdef PETSC_HAVE_EXODUSII
+	Call DMMeshCreateExodus(PETSC_COMM_WORLD,infile,mesh,ierr);CHKERRQ(ierr)
+	
+   Call PetscOptionsGetString(PETSC_NULL_CHARACTER,'-o',outfile,flg,ierr);CHKERRQ(ierr)
+	If (flg) Then
+		Call PetscViewerBinaryOpen(PETSC_COMM_WORLD,outfile,FILE_MODE_WRITE,meshViewer,ierr);CHKERRQ(ierr)
+		Call DMView(mesh,meshViewer,ierr); CHKERRQ(ierr)
+		Call PetscViewerDestroy(meshViewer,ierr);CHKERRQ(ierr)
+	End If
+#else
+	Call PetscViewerBinaryOpen(PETSC_COMM_WORLD,infile,FILE_MODE_READ,meshViewer,ierr);CHKERRQ(ierr)
+	Call DMMeshLoad(mesh,meshViewer,ierr); CHKERRQ(ierr)
+	Call PetscViewerDestroy(meshViewer,ierr);CHKERRQ(ierr)
+#endif
+	
+	Call DMMeshGetCoordinatesF90(mesh,Coord,ierr);CHKERRQ(ierr)
+	Write(*,*) 'Coordinates: '
+	Write(*,*) Coord
+	Call DMMeshRestoreCoordinatesF90(mesh,Coord,ierr);CHKERRQ(ierr)
+	
+	Call DMMeshGetElementsF90(mesh,connect,ierr);CHKERRQ(ierr)
+	write(*,*) 'Connect: '
+	write(*,*) connect
+	Call DMMeshRestoreElementsF90(mesh,connect,ierr);CHKERRQ(ierr)
+	
+	Call DMDestroy(mesh,ierr);CHKERRQ(ierr)
+	Call PetscFinalize(ierr)
 End Program TestNewSieve
