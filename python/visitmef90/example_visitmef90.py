53ff727478860cefe25b5715a550cd9669735254
corrado maurini <corado.maurini@upmc.fr>
2011-07-08 16:31:31 +0200

    exemple

---
+++ python/visitmef90/example_visitmef90.py
@@ -0,0 +1,27 @@
+#
+# run as:  visit -cli -s example_visitmef90.py
+# Author corrado.maurini@upmc.fr
+#
+from visit import *
+import os; import sys; import glob; 
+import pymef90;
+import visitmef90 as vm
+
+# gen prefix
+prefix='Slab2DNG-QTri-X100-Y60-h3'
+
+# get last step
+nsteps = pymef90.getlaststep(prefix+'.ener')
+
+# Represt Fracture 
+vm.FigureFracture(prefix,nsteps-1) 
+
+# Save image of a single time step 
+vm.ExportSingleFigure(prefix,nsteps-1) 
+
+# Save images of all time steps 
+vm.ExportTimeFigure(prefix)    
+
+# Exit
+exit()
+
