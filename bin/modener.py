e12905b8e0144dda19a83e2e969d65196e80a2bf
Blaise Bourdin <bourdin@lsu.edu>
2011-07-28 10:07:12 -0500

    Same for modener

--- bin/modener.py
+++ bin/modener.py
@@ -1,36 +1,43 @@
 #!/usr/bin/env python
-import pymef90
-import numpy as np
-from optparse import OptionParser
+def parse(args=None):
+    import argparse
+    import sys
+    ### Get options from the command line
+    parser = argparse.ArgumentParser(description='Plot energy evolution for VarFracQS.')
+    parser.add_argument("-f","--forces",default=False,action="store_true",help="displays forces work")
+    parser.add_argument('inputfile',nargs='?',type=argparse.FileType('r'),help='Input file',default=sys.stdin)
+    parser.add_argument('outputfile',nargs='?',type=argparse.FileType('r'),help='(optional) output file',default=sys.stdout)
+    parser.add_argument("-d","--debug",action="store_true",default=False,help="Display useless debugging information")
+    parser.add_argument("-M","--stepmax",type=int,help="last time step")
+    parser.add_argument("-o","--old",action="store_true",default=False,help="old style energy file (no forces)")
+    parser.add_argument("-r","--relative",action="store_true",default=False,help="offset surface energy")
+    return parser.parse_args()
 
-### Get options from the command line
-parser = OptionParser()
-parser.add_option("-d", "--debug", dest="debug", action="store_true", default=False, help="display useless debugging information")
-parser.add_option("-i", "--input", dest="inputfile", help="energy file")
-parser.add_option("-o", "--output", dest="outputfile", help="output file name")
-parser.add_option("--old", dest="old", action="store_true", default=False, help="old style energy file (no forces)")
-parser.add_option("-M", "--stepmax", dest="stepmax", default=None, help="step to parse until" )
 
+def main():
+    import pymef90
+    import matplotlib
+    import numpy as np
+    options = parse()
 
-(options, args) = parser.parse_args()
-if options.debug:
-  print("Option table: {0}".format(options))
-if options.inputfile == None:
-  parser.error("must specify input file with -i or --input")
+    if options.debug:
+      print("Option table: {0}".format(options))
+    if options.old:
+      energies_old=np.loadtxt(options.inputfile)
+      energies=np.zeros( (energies_old.shape[0], 6) )
+      energies[:,:3]=energies_old[:,:3]
+      energies[:,-2:]=energies_old[:,-2:]
+    else:
+      energies=np.loadtxt(options.inputfile)
 
-### Read input file
-if options.old:
-  energies_old=np.loadtxt(options.inputfile)
-  energies=np.zeros( (energies_old.shape[0], 6) )
-  energies[:,:3]=energies_old[:,:3]
-  energies[:,-2:]=energies_old[:,-2:]
-else:
-  energies=np.loadtxt(options.inputfile)
+    if options.debug:
+        print('size of energies: {0}'.format(energies.shape))
+        print('Energies: {0}'.format(energies))
 
-if options.debug:
-  print('size of energies: {0}'.format(energies.shape))
-  print('Energies: {0}'.format(energies))
+    energiesBT=pymef90.energies.fixBT(energies, options.stepmax)
+    
+    pymef90.energies.save(options.outputfile, energiesBT)
 
-energiesBT=pymef90.energies.fixBT(energies, options.stepmax)
-
-pymef90.energies.save(options.outputfile, energiesBT)
+if __name__ == "__main__":
+    import sys
+    sys.exit(main())
