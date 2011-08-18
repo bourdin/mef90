085a2e0d80cbd85d1c703ad07b4d75811557581b
Blaise Bourdin <bourdin@lsu.edu>
2011-08-18 12:00:04 -0500

    BF are OK, Grad_BF are not

--- Tests/TestBoundaryElement3D.F90
+++ Tests/TestBoundaryElement3D.F90
@@ -15,7 +15,7 @@ Program TestBoundaryElement3D
    PetscInt                                     :: num_vert,num_vert_set
    PetscInt                                     :: num_side,num_side_set
    PetscInt                                     :: num_dim
-   PetscReal, Dimension(:), Pointer             :: UScal
+   PetscReal, Dimension(:), Pointer             :: U1,U2
    Type(Vect3D), Dimension(:), Pointer          :: UVect
    PetscReal                                    :: average2,average3,flux
    Real, Dimension(:), Pointer                  :: X,Y,Z
@@ -25,9 +25,8 @@ Program TestBoundaryElement3D
    Type(BoundaryElement3d_Scal)                 :: bElem
    Type(Element2d_Scal)                         :: Elem
    Type(PetscViewer)                            :: viewer
-   PetscInt                                     :: IntegOrder = 2
+   PetscInt                                     :: IntegOrder = 3
    PetscReal                                    :: area2,area3
-   PetscReal                                    :: inc2,inc3
    
    Integer                                      :: cpu_ws,io_ws,mod_sz,exoid
    Real                                         :: vers
@@ -65,25 +64,30 @@ Program TestBoundaryElement3D
    Call exgelc (exoid,1,tmpconnect,ierr)
    connect = reshape(tmpconnect,(/3,num_elem/))
    !Write(*,*) 'Connect: '
-   Do i = 1, num_Elem
-      Write(*,*) i, connect(:,i)
-   End Do
+   !Do i = 1, num_Elem
+   !   Write(*,*) i, connect(:,i)
+   !End Do
    
-   Allocate(UScal(num_vert))
-   Allocate(UVect(num_vert))
+   Allocate(U1(num_vert))
+   Allocate(U2(num_vert))
    Do i = 1, num_vert
-      UScal(i) = 1.0_Kr
-      UVect(i)%X = 0.0_Kr
-      UVect(i)%Y = 0.0_Kr
-      UVect(i)%Z = 1.0_Kr
+      U1(i) = Y(i)
+      U2(i) = Z(i)
    End Do
    
    average2 = 0.0_Kr
    average3 = 0.0_Kr
    flux    = 0.0_Kr
+   
+   iE = 1
+   Coord3(1,:) = X(connect(:,iE))
+   Coord3(2,:) = Y(connect(:,iE))
+   Coord3(3,:) = Z(connect(:,iE))
+   Call BoundaryElement_Init(bElem,Coord3,IntegOrder,MEF90_P1_Lagrange)
+   Call BoundaryElement_View(bElem,PetscViewer(PETSC_VIEWER_STDOUT_WORLD))
+   Call BoundaryElement_Destroy(bElem)
+   
    Do iE = 1, num_Elem
-      area3 = 0.0_Kr
-      inc3 = 0.0_Kr
       Coord3(1,:) = X(connect(:,iE))
       Coord3(2,:) = Y(connect(:,iE))
       Coord3(3,:) = Z(connect(:,iE))
@@ -93,14 +97,11 @@ Program TestBoundaryElement3D
       Do iG = 1, Num_Gauss
          area3  = area3 + bElem%Gauss_C(iG)
          Do iDof = 1, Num_DoF
-            inc3 = inc3 + bElem%Gauss_C(iG) * (bElem%BF(iDoF,iG) * UScal(connect(iE,iDoF)))
+            average3 = average3 + bElem%Gauss_C(iG) * (bElem%BF(iDoF,iG) * U1(connect(iE,iDoF)))
+            flux     = flux + bElem%Gauss_C(iG) * (bElem%Grad_BF(iDoF,iG) .DotP. bElem%NormalVector) * U2(connect(iE,iDoF))
          End Do
       End Do
-      average3 = average3 + inc3
-      !Write(*,*) 'Area (boundary)', sum(bElem%Gauss_C)
       
-      area2 = 0.0_Kr
-      inc2 = 0.0_Kr
       Coord2(1,:) = X(connect(:,iE))
       Coord2(2,:) = Y(connect(:,iE))
       Call ElementInit(Elem,Coord2,IntegOrder,MEF90_P1_Lagrange)
@@ -109,19 +110,17 @@ Program TestBoundaryElement3D
       Do iG = 1, Num_Gauss
          area2 = area2 + Elem%Gauss_C(iG)
          Do iDof = 1, Num_DoF
-            inc2  = inc2 + Elem%Gauss_C(iG) * (Elem%BF(iDoF,iG) * UScal(connect(iE,iDoF)))
+            average2  = average2 + Elem%Gauss_C(iG) * (Elem%BF(iDoF,iG) * U1(connect(iE,iDoF)))
          End Do
       End Do
-      average2 = average2 + inc2
-      !Write(*,*) 'Area (Body)', sum(Elem%Gauss_C)
-      !Write(*,*) 'inc ratio ', inc2 / inc3
-      !Write(*,*) 'area ratio ', area2 / area3
       Call BoundaryElement_Destroy(bElem)
       Call ElementDestroy(Elem)
    End Do
+   Write(*,*) 'Area2 (using 2D body elements) is     ', area2
+   Write(*,*) 'Area3 (using 3D boundary elements) is ', area3
    Write(*,*) 'Average2 (using 2D body elements) is     ', average2
    Write(*,*) 'Average3 (using 3D boundary elements) is ', average3
-   
+   Write(*,*) 'Flux is ', flux   
    DeAllocate(X)
    DeAllocate(Y)
    DeAllocate(Z)
@@ -129,8 +128,8 @@ Program TestBoundaryElement3D
    DeAllocate(Coord3)
    DeAllocate(tmpconnect)
    DeAllocate(connect)
-   DeAllocate(UScal)
-   DeAllocate(UVect)
+   DeAllocate(U1)
+   DeAllocate(U2)
    Call exclos(exoid,ierr)   
    Call MEF90_Finalize()
 End Program TestBoundaryElement3D
