e8b70de7fc2d1b2ab5ee6c513f3f7365da337a50
Andres Leon Baldelli @muspratt <a.leon.baldelli@gmail.com>
2011-11-17 11:19:44 +0100

    Added tiny script makeallfilm.sh

---
+++ makeallfilm.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+make -C MEF90              
+make -C m_VarFilm_Struct   
+make -C PrepFilm           
+make -C VarFilmQS          
+                           
+                           
