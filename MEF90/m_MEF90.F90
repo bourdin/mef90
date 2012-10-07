d3266031eb038c59b7df38f793f94c6f71f29273
Blaise Bourdin <bourdin@lsu.edu>
2012-10-07 07:30:32 -0500

    Continuing work on MEF90Ctx

--- MEF90/m_MEF90.F90
+++ MEF90/m_MEF90.F90
@@ -14,30 +14,40 @@ Module m_MEF90
    Use m_MEF_Sieve
    Use m_MEF_Utils
 
+   Implicit NONE
    Public :: MEF90_Initialize
    Public :: MEF90_Finalize
+   Public :: MEF90CtxBag
+   
+   PetscBag                                        :: MEF90CtxBag 
    
 Contains
 #undef __FUNCT__
 #define __FUNCT__ "MEF90_Initialize"
-   Subroutine MEF90_Initialize()
-      PetscInt                      :: ierr
+   Subroutine MEF90_Initialize(MEF90Ctx,ierr)
+      PetscInt,Intent(OUT)                               :: ierr
+      Type(MEF90Ctx_Type),pointer                        :: MEF90Ctx
        
       Call PetscInitialize(PETSC_NULL_CHARACTER,ierr);CHKERRQ(ierr)
-      Call MEF90MPI_InitializePrivate(ierr);CHKERRQ(ierr)
-
       Call PetscLogBegin(ierr);CHKERRQ(ierr)
+
       
       !!! Individual modules runtime initialization should be called here
+      Call MEF90MPI_InitializePrivate(ierr);CHKERRQ(ierr)
       Call MEF90Materials_InitializePrivate(ierr);CHKERRQ(ierr)
       Call MEF90Ctx_InitializePrivate(ierr);CHKERRQ(ierr)
+      
+      Call PetscBagCreate(PETSC_COMM_WORLD,sizeofMEF90Ctx,MEF90CtxBag,ierr)
+      Call PetscBagGetDataMEF90Ctx(MEF90CtxBag,MEF90Ctx,ierr);CHKERRQ(ierr)  
    End Subroutine MEF90_Initialize
+   
    
 #undef __FUNCT__
 #define __FUNCT__ "MEF90_Finalize"
    Subroutine MEF90_Finalize()
-      PetscInt                         :: ierr
+      PetscInt                                     :: ierr
       
+      Call PetscBagDestroy(MEF90CtxBag,ierr)
       Call MEF90MPI_FinalizePrivate(ierr);CHKERRQ(ierr)
       Call PetscFinalize(ierr)
    End Subroutine MEF90_Finalize
