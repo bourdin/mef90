bd95d6ea7749f8efe7ff17c2c1227426264e8ca0
Blaise Bourdin <bourdin@lsu.edu>
2013-03-20 12:18:08 -0500

    fixed a bug when visit module not importable

--- python/pymef90/__init__.py
+++ python/pymef90/__init__.py
@@ -3,4 +3,8 @@ from cubmef90 import *
 from infotxt import *
 from jobs import *
 from pymef90 import *
-import visitmef90
+try:
+    import visitmef90
+except ImportError:
+    print "visit not available, not importing visitmef90"
+
