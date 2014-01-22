2db6b3c3d3b83257fb90b05f19a1396e5f6463b7
Blaise Bourdin <bourdin@lsu.edu>
2014-01-22 10:18:14 -0600

    Finished surfing BC pre-processor

--- bin/getchangesets.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Q&D to retrieve the changesets of the sensitive packages. 
-echo "PETSc-dev changeset:" ` cd $PETSC_DIR && hg log -l 1 | grep changeset | awk '{print $2}'`
-echo "	Builder changeset:" ` cd $PETSC_DIR/config/BuildSystem/ && hg log -l 1 | grep changeset | awk '{print $2}'`
-echo "MEF90 changeset:" ` cd $MEF90_DIR && hg log -l 1 | grep changeset | awk '{print $2}'`
-echo "Tao-lite changeset:" ` cd $TAO_DIR && hg log -l 1 | grep changeset | awk '{print $2}'`
