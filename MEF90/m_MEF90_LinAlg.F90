afb59e5c30a6aeb50eff13f5c4954f4d93e1755f
Blaise Bourdin <bourdin@lsu.edu>
2014-07-07 10:11:48 -0500

    code formatting

--- MEF90/m_MEF90_LinAlg.F90
+++ MEF90/m_MEF90_LinAlg.F90
@@ -110,23 +110,23 @@ Module m_MEF90_LinAlg
    Type(Vect3D),Parameter    :: MEF90Vect3De3 = Vect3D(0.0_Kr,0.0_Kr,1.0_Kr)
    
    Type(Mat2D),Parameter     :: MEF90Mat2DIdentity = Mat2D(1.0_Kr,0.0_Kr,          &
-                                                             0.0_Kr,1.0_Kr)
+                                                           0.0_Kr,1.0_Kr)
    Type(MatS2D),Parameter    :: MEF90MatS2DIdentity = MatS2D(1.0_Kr,1.0_Kr,0.0_Kr)
    Type(Mat3D),Parameter     :: MEF90Mat3DIdentity = Mat3D(1.0_Kr,0.0_Kr,0.0_Kr,   &
-                                                             0.0_Kr,1.0_Kr,0.0_Kr,   &
-                                                             0.0_Kr,0.0_Kr,1.0_Kr)
+                                                           0.0_Kr,1.0_Kr,0.0_Kr,   &
+                                                           0.0_Kr,0.0_Kr,1.0_Kr)
    Type(MatS3D),Parameter    :: MEF90MatS3DIdentity = MatS3D(1.0_Kr,1.0_Kr,1.0_Kr, &
-                                                               0.0_Kr,0.0_Kr,0.0_Kr)
+                                                             0.0_Kr,0.0_Kr,0.0_Kr)
 
    Type(Tens4OS2D),Parameter :: MEF90Tens4OS2DIdentity = Tens4OS2D(1.0_Kr,0.0_Kr,0.0_Kr, &
-                                                                     1.0_Kr,0.0_Kr,        & 
-                                                                     1.0_Kr)
+                                                                   1.0_Kr,0.0_Kr,        & 
+                                                                   1.0_Kr)
    Type(Tens4OS3D),Parameter :: MEF90Tens4OS3DIdentity = Tens4OS3D(1.0_Kr,0.0_Kr,0.0_Kr,0.0_Kr,0.0_Kr,0.0_Kr, & ! 1111,1112,1113,1122,1123,1133
-                                                                     1.0_Kr,0.0_Kr,0.0_Kr,0.0_Kr,0.0_Kr,        & ! 1212,1213,1222,1223,1233
-                                                                     1.0_Kr,0.0_Kr,0.0_Kr,0.0_Kr,               & ! 1313,1322,1323,1333
-                                                                     1.0_Kr,0.0_Kr,0.0_Kr,                      & ! 2222,2223,2233
-                                                                     1.0_Kr,0.0_Kr,                             & ! 2323,2333
-                                                                     1.0_Kr)                                      ! 3333
+                                                                   1.0_Kr,0.0_Kr,0.0_Kr,0.0_Kr,0.0_Kr,        & ! 1212,1213,1222,1223,1233
+                                                                   1.0_Kr,0.0_Kr,0.0_Kr,0.0_Kr,               & ! 1313,1322,1323,1333
+                                                                   1.0_Kr,0.0_Kr,0.0_Kr,                      & ! 2222,2223,2233
+                                                                   1.0_Kr,0.0_Kr,                             & ! 2323,2333
+                                                                   1.0_Kr)                                      ! 3333
 
    Interface Operator (+)
       Module Procedure SumVect2D,SumVect3D,SumMat2D,SumMat3D,SumMatS2D,SumMatS3D,SumTens4OS2D,SumTens4OS3D
