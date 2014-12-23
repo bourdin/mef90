2b16d4afbc9ac073d88976ed804378cb664031b3
Blaise Bourdin <bourdin@lsu.edu>
2014-12-23 11:05:53 -0600

    Moved default conmtext values back into vDef as it seems to confuse intel 14 compilers

--- vDef/vDef.F90
+++ vDef/vDef.F90
@@ -1,13 +1,13 @@
 #include "../MEF90/mef90.inc"
 Program vDef
 #include <finclude/petscdef.h>
+   Use petsc
    Use m_MEF90
+   !Use m_vDef
    Use m_MEF90_DefMechCtx
    Use m_MEF90_DefMech
-   Use m_MEF90_HeatXfer
    Use m_MEF90_HeatXferCtx
-   Use m_vDef
-   Use petsc
+   Use m_MEF90_HeatXfer
    Implicit NONE   
 
    PetscErrorCode                                     :: ierr
@@ -60,20 +60,119 @@ Program vDef
    Integer                                            :: step
    PetscInt                                           :: dim
    
-   PetscReal                                          :: alphaMaxChange,alphamIn,alphaMax
+   PetscReal                                          :: alphaMaxChange,alphaMin,alphaMax
    
    PetscBool                                          :: BTActive = Petsc_False
    PetscInt                                           :: BTStep,BTminStep,BTMaxSTep,BTDirection
       
+!!! Default values of the contexts
+   Type(MEF90CtxGlobalOptions_Type),Parameter         :: vDefDefaultGlobalOptions = MEF90CtxGlobalOptions_Type( &
+                                                         1,                             & ! verbose
+                                                         PETSC_FALSE,                   & ! validate
+                                                         MEF90TimeInterpolation_linear, & ! timeInterpolation
+                                                         0.0_Kr,                        & ! timeMin
+                                                         1.0_Kr,                        & ! timeMax
+                                                         11,                            & ! timeNumStep
+                                                         MEF90FileFormat_EXOSingle)       ! fileFormat
+
+   Type(MEF90DefMechGlobalOptions_Type),Parameter     :: vDefDefMechDefaultGlobalOptions2D = MEF90DefMechGlobalOptions_Type( &
+                                                         MEF90DefMech_ModeQuasiStatic, & ! mode
+                                                         PETSC_TRUE,              & ! disp_addNullSpace
+                                                         3,                       & ! DisplacementOffset
+                                                         2,                       & ! DamageOffset
+                                                         3,                       & ! boundaryDisplacementOffset
+                                                         0,                       & ! boundaryDamageOffset
+                                                         1,                       & ! temperatureOffset
+                                                         4,                       & ! ForceOffset
+                                                         3,                       & ! pressureForceOffset
+                                                         0,                       & ! plasticStrainOffset
+                                                         6,                       & ! StressOffset
+                                                         MEF90Scaling_Linear,     & ! boundaryDisplacementScaling
+                                                         MEF90Scaling_CST,        & ! boundaryDamageScaling
+                                                         MEF90Scaling_Linear,     & ! ForceScaling
+                                                         MEF90Scaling_Linear,     & ! pressureForceScaling
+                                                         1e-4,                    & ! damage_atol
+                                                         1000,                    & ! maxit
+                                                         0.,                      & ! irrevThres 
+                                                         MEF90DefMech_BTTypeNULL, & ! BTType
+                                                         -1,                      & ! BTInt
+                                                         -1,                      & ! BTScope
+                                                         1.0e-2)                    ! BTTol
+   Type(MEF90DefMechGlobalOptions_Type),Parameter     :: vDefDefMechDefaultGlobalOptions3D = MEF90DefMechGlobalOptions_Type( &
+                                                         MEF90DefMech_ModeQuasiStatic, & ! mode
+                                                         PETSC_TRUE,              & ! disp_addNullSpace
+                                                         3,                       & ! DisplacementOffset
+                                                         2,                       & ! DamageOffset
+                                                         3,                       & ! boundaryDisplacementOffset
+                                                         0,                       & ! boundaryDamageOffset
+                                                         1,                       & ! temperatureOffset
+                                                         4,                       & ! ForceOffset
+                                                         3,                       & ! pressureForceOffset
+                                                         0,                       & ! plasticStrainOffset
+                                                         7,                       & ! StressOffset
+                                                         MEF90Scaling_Linear,     & ! boundaryDisplacementScaling
+                                                         MEF90Scaling_CST,        & ! boundaryDamageScaling
+                                                         MEF90Scaling_Linear,     & ! ForceScaling
+                                                         MEF90Scaling_Linear,     & ! pressureForceScaling
+                                                         1e-4,                    & ! damage_atol
+                                                         1000,                    & ! maxit
+                                                         0.,                      & ! irrevThres 
+                                                         MEF90DefMech_BTTypeNULL, & ! BTType
+                                                         -1,                      & ! BTInt
+                                                         -1,                      & ! BTScope
+                                                         1.0e-2)                    ! BTTol
+
+   Type(MEF90DefMechCellSetOptions_Type),Parameter    :: vDefDefMechDefaultCellSetOptions = MEF90DefMechCellSetOptions_Type( &
+                                                         -1,                                      & ! elemTypeShortIDDispl will be overriden
+                                                         -1,                                      & ! elemTypeShortIDDamage will be overriden
+                                                         [0.0_Kr,0.0_Kr,0.0_Kr],                  & ! force
+                                                         0.0_Kr,                                  & ! pressureForce
+                                                         MEF90DefMech_damageTypeAT1,              & ! damageType
+                                                         MEF90DefMech_plasticityTypeNone,         & ! plasticityType
+                                                         MEF90DefMech_unilateralContactTypeNone,  & ! unilateralContactType
+                                                         [PETSC_FALSE,PETSC_FALSE,PETSC_FALSE],   & ! Has Displacement BC
+                                                         [0.0_Kr,0.0_Kr,0.0_Kr],                  & ! boundary Displacement
+                                                         PETSC_FALSE,                             & ! Has Damage BC
+                                                         0._Kr)                                     ! Boundary Damage
+   Type(MEF90DefMechVertexSetOptions_Type),Parameter  :: vDefDefMechDefaultVertexSetOptions = MEF90DefMechVertexSetOptions_Type( &
+                                                         [PETSC_FALSE,PETSC_FALSE,PETSC_FALSE],   & ! Has Displacement BC
+                                                         [0.0_Kr,0.0_Kr,0.0_Kr],                  & ! boundary Displacement
+                                                         PETSC_FALSE,                             & ! Has Damage BC
+                                                         0.0_Kr)                                    ! boundary Damage
+
+   Type(MEF90HeatXferGlobalOptions_Type),Parameter    :: vDefHeatXferDefaultGlobalOptions = MEF90HeatXferGlobalOptions_Type( &
+                                                         MEF90HeatXFer_ModeSteadyState, & ! mode
+                                                         PETSC_FALSE,         & ! addNullSpace
+                                                         1,                   & ! tempOffset
+                                                         0.,                  & ! initialTemperature
+                                                         MEF90Scaling_Linear, & ! boundaryTempScaling
+                                                         0,                   & ! boundaryTempOffset
+                                                         MEF90Scaling_Linear, & ! externalTempScaling
+                                                         2,                   & ! externalTempOffset
+                                                         MEF90Scaling_Linear, & ! fluxScaling
+                                                         1)                     ! fluxOffset
+   Type(MEF90HeatXferCellSetOptions_Type),Parameter   :: vDefHeatXferDefaultCellSetOptions = MEF90HeatXferCellSetOptions_Type( &
+                                                         -1,            & ! elemTypeShortID will be overriden
+                                                         0.0_Kr,        & ! flux
+                                                         0.0_Kr,        & ! surfaceThermalConductivity
+                                                         0.0_Kr,        & ! externalTemp
+                                                         PETSC_FALSE,   & ! Has BC
+                                                         0.0_Kr)          ! boundaryTemp
+                                                         
+   Type(MEF90HeatXferVertexSetOptions_Type),Parameter :: vDefHeatXferDefaultVertexSetOptions = MEF90HeatXferVertexSetOptions_Type( &
+                                                         PETSC_FALSE,   & ! Has BC
+                                                         0.0_Kr)          ! boundaryTemp
+
+
    !!! Initialize MEF90
    Call PetscInitialize(PETSC_NULL_CHARACTER,ierr)
    !!!Call TAOInitialize(PETSC_NULL_CHARACTER,ierr);CHKERRQ(ierr)
    Call MEF90Initialize(ierr)
    Call PetscPrintf(PETSC_COMM_WORLD," # vDef: numerical implementation of variational models of Defect Mechanics\n",ierr);CHKERRQ(ierr)
    
-
+   
    !!! Get all MEF90-wide options
-   Call MEF90CtxCreate(PETSC_COMM_WORLD,MEF90Ctx,MEF90DefaultGlobalOptions,ierr);CHKERRQ(ierr)
+   Call MEF90CtxCreate(PETSC_COMM_WORLD,MEF90Ctx,vDefDefaultGlobalOptions,ierr);CHKERRQ(ierr)
    Call PetscBagGetDataMEF90CtxGlobalOptions(MEF90Ctx%GlobalOptionsBag,MEF90GlobalOptions,ierr);CHKERRQ(ierr)
 
    !!! Get DM from mesh
@@ -88,19 +187,19 @@ Program vDef
    !!! Create DefMech context, get all DefMech options
    Call MEF90DefMechCtxCreate(MEF90DefMechCtx,Mesh,MEF90Ctx,ierr);CHKERRQ(ierr)
    If (dim == 2) Then
-      Call MEF90DefMechCtxSetFromOptions(MEF90DefMechCtx,PETSC_NULL_CHARACTER,MEF90DefMechDefaultGlobalOptions2D, &
-                                         MEF90DefMechDefaultCellSetOptions,MEF90DefMechDefaultVertexSetOptions,ierr)
+      Call MEF90DefMechCtxSetFromOptions(MEF90DefMechCtx,PETSC_NULL_CHARACTER,vDefDefMechDefaultGlobalOptions2D, &
+                                         vDefDefMechDefaultCellSetOptions,vDefDefMechDefaultVertexSetOptions,ierr)
    Else
-      Call MEF90DefMechCtxSetFromOptions(MEF90DefMechCtx,PETSC_NULL_CHARACTER,MEF90DefMechDefaultGlobalOptions3D, &
-                                         MEF90DefMechDefaultCellSetOptions,MEF90DefMechDefaultVertexSetOptions,ierr)
+      Call MEF90DefMechCtxSetFromOptions(MEF90DefMechCtx,PETSC_NULL_CHARACTER,vDefDefMechDefaultGlobalOptions3D, &
+                                         vDefDefMechDefaultCellSetOptions,vDefDefMechDefaultVertexSetOptions,ierr)
    End If
    Call PetscBagGetDataMEF90DefMechCtxGlobalOptions(MEF90DefMechCtx%GlobalOptionsBag,MEF90DefMechGlobalOptions,ierr);CHKERRQ(ierr)
    
    
    !!! Create HeatXfer context, get all HeatXfer options
    Call MEF90HeatXferCtxCreate(MEF90HeatXferCtx,Mesh,MEF90Ctx,ierr);CHKERRQ(ierr)
-   Call MEF90HeatXferCtxSetFromOptions(MEF90HeatXferCtx,PETSC_NULL_CHARACTER,MEF90HeatXferDefaultGlobalOptions, &
-                                       MEF90HeatXferDefaultCellSetOptions,MEF90HeatXferDefaultVertexSetOptions,ierr)
+   Call MEF90HeatXferCtxSetFromOptions(MEF90HeatXferCtx,PETSC_NULL_CHARACTER,vDefHeatXferDefaultGlobalOptions, &
+                                       vDefHeatXferDefaultCellSetOptions,vDefHeatXferDefaultVertexSetOptions,ierr)
    Call PetscBagGetDataMEF90HeatXferCtxGlobalOptions(MEF90HeatXferCtx%GlobalOptionsBag,MEF90HeatXferGlobalOptions,ierr);CHKERRQ(ierr)
 
    !!! Get material properties bags
