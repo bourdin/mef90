03dfee75a2ed65dc09e679bb095cd4b8d2224ec0
Erwan TANNE <erwan.tanne@polytechnique.fr>
2017-04-04 17:58:02 -0500

    Add comments, and modifications to do for pressurized cracks.

--- bin/G_theta.py
+++ bin/G_theta.py
@@ -14,6 +14,7 @@ def parse(args=None):
     parser.add_argument("--R_out",type=float,help="outer radius of the control area",default=1.)
     parser.add_argument('--Angle_crack_propagation',type=float,help="crack propagation angle made with the x direction in degree",default=0.)
     parser.add_argument('--Crack_position',type=float,nargs=2,default=[0,0],help='postion of the crack in X,Y')
+    #parser.add_argument('--cs',type=int,nargs='*',default=1,help='set of blocks where G theta is applied, check on visit to get the number of the set')
     return parser.parse_args()
 
 
@@ -34,18 +35,33 @@ def Get_G(opts):
     print "Trying to open database %s"%MyDatabase
     status = OpenDatabase(MyDatabase)
     
-    ## Define displacement, stress, and strain
+    print "Assumptions: \n"
+    print "(i) 2D domain with a straigth crack (can be extended to a 3D, but it is not supported in this version)\n"
+    print "(ii) homogenous material and cracks stay in the same material property. Do not apply if the crack cross different material properties \n"
+    print "(iii) free stress at crack lips + no lips interpentration (it can be expressed with pressure applied on crack lips, but it is not supported in this version)\n"
+    print "(iv) no force density (it can be expressed with force density, but it is not supported in this version )\n"
+
+    #### Define displacement, stress, and strain
     DefineVectorExpression("Displacement_2D", "{Displacement_X,Displacement_Y}")
     DefineTensorExpression("Stress_Tensor_2D", "{{Stress_XX,Stress_XY},{Stress_XY,Stress_YY}}")
     DefineTensorExpression("GradU", "{{gradient(Displacement_2D[0])[0],gradient(Displacement_2D[0])[1]},{gradient(Displacement_2D[1])[0],gradient(Displacement_2D[1])[1]}}")
 
-    ## define the controlled area for the 
+    #### define the controlled area for the 
     DefineVectorExpression("Crack_Pos", '{'+str(opts.Crack_position[0])+','+str(opts.Crack_position[1])+'}')
     DefineVectorExpression("Coords", "coord(Mesh)-Crack_Pos")
     DefineScalarExpression("R", "sqrt( (Coords[0])^2.+(Coords[1])^2.)")
     DefineVectorExpression("InnerOuterRadius", '{'+str(opts.R_inn)+','+str(opts.R_out)+'}')
 
-    ## Define the vector theta and its gradients
+    #### adding terms for force density = -Crack_Pressure*gradient(Damage)
+    #DefineVectorExpression("ThetaVector", "-Crack_Pressure*gradient(Damage)")
+    #DefineVectorExpression("GradFdotThetaVector","{gradient(CrackPressureForce[0])[0]*ThetaVector[0]+gradient(CrackPressureForce[0])[1]*ThetaVector[0],gradient(CrackPressureForce[1])[0]*ThetaVector[0]+gradient(CrackPressureForce[1])[1]*ThetaVector[1]}")
+    #DefineScalarExpression("G_theta_Density","trace(Stress_Tensor_2D*(GradU*GradThetaVector))-.5*trace(Stress_Tensor_2D*GradU)*divergence(ThetaVector)+dot(CrackPressureForce,Displacement_2D)*divergence(ThetaVector)+dot(Displacement_2D,GradFdotThetaVector)")
+
+    #### adding terms when crack is pressurized
+    #### G_theta_Density + int_{\Gamma(Rout)-\Gamma(Rint])} Gamma_pressure_crack ds
+    # DefineScalarExpression("Gamma_pressure_crack","dot(normale_crack*Displacement_2D)*divergence(ThetaVector)")
+
+    #### Define the vector theta and its gradients
     DefineScalarExpression("Theta_function", "if( le(R, InnerOuterRadius[0]), 1 , if(ge(R, InnerOuterRadius[1]), 0, (R - InnerOuterRadius[1] )/( InnerOuterRadius[0] - InnerOuterRadius[1]) ))")
     DefineVectorExpression("ThetaVector", '{Theta_function*cos('+str(opts.Angle_crack_propagation*180/math.pi)+'),Theta_function*sin('+str(opts.Angle_crack_propagation*180/math.pi)+')}')
     DefineTensorExpression("GradThetaVector", "{{gradient(ThetaVector[0])[0],gradient(ThetaVector[0])[1]},{gradient(ThetaVector[1])[0],gradient(ThetaVector[1])[1]}}")
@@ -53,7 +69,16 @@ def Get_G(opts):
     DefineScalarExpression("G_theta_Density", "trace(Stress_Tensor_2D*(GradU*GradThetaVector))-.5*trace(Stress_Tensor_2D*GradU)*divergence(ThetaVector)")
 
 
+
     AddPlot("Pseudocolor","G_theta_Density")
+    #silr = SILRestriction()
+    #silr.SuspendCorrectnessChecking()
+    #silr.TurnOffAll()
+    #for var_cs in opts.cs:
+    #    silr.TurnOnSet(var_cs)
+    #silr.EnableCorrectnessChecking()
+    #SetPlotSILRestriction(silr ,1)
+
     DrawPlots()
     SuppressQueryOutputOn()
 
