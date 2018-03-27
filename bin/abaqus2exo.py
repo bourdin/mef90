5e4370db9a640d3a6642265e8c78e60d6df449b5
Blaise Bourdin <bourdin@lsu.edu>
2018-03-27 15:12:43 -0500

    Updated abaqus to exodus script

--- bin/abaqus2exo.py
+++ bin/abaqus2exo.py
@@ -65,6 +65,8 @@ def ABAQUSImporter(filename):
                "CPE4R":4,    # 4 node quad
                "CPE8":8,     # 8 node quad
                "CPE8S":8,    # 8 node quad
+               "CPS3":3,     # 3 node tri
+               "CPS6":6,     # 6 node tri
                "CPS4":4,     # 4 node quad
                "CPS4S":4,    # 4 node quad
                "CPS4R":4,    # 4 node quad
@@ -74,7 +76,6 @@ def ABAQUSImporter(filename):
                "C3D8":8,     # 8 node hex
                "C3D20":20,   # 20 node hex
                "C3D20S":20,  # 20 node hex
-               "CPS3":3      # 3 node line
               }
     elemType = {
                 "S3R":"TRI3",       # 3 node triangle
@@ -91,6 +92,8 @@ def ABAQUSImporter(filename):
                 "CPE4":"QUAD4",     # 4 node quad
                 "CPE4S":"QUAD4",    # 4 node quad
                 "CPE4R":"QUAD4",    # 4 node quad
+                "CPS3":"TRI3",      # 3 node triangle
+                "CPS6":"TRI6",      # 6 node triangle
                 "CPS4":"QUAD4",     # 4 node quad
                 "CPS4S":"QUAD4",    # 4 node quad
                 "CPS4R":"QUAD4",    # 4 node quad
@@ -102,7 +105,6 @@ def ABAQUSImporter(filename):
                 "C3D8":"HEX8",      # 8 node hex
                 "C3D20":"HEX20",    # 20 node hex
                 "C3D20S":"HEX20",   # 20 node hex
-                "CPS3":"TRI3"       # 3 node triangle
                 }
 
     # Opening and reading abaqus file
@@ -116,15 +118,17 @@ def ABAQUSImporter(filename):
             sys.stdout.write( "Parsed {0} \n".format(line.strip()))
             (coord,nodeID,line,f,order) = readCoords(f,line)
         elif line.startswith('*ELEM'):
-            sys.stdout.write( "Parsed {0} \n".format(line.strip()))
+            sys.stdout.write( "Parsed {0} ".format(line.strip()))
+            #sys.stdout.write( "elemType: {0}".format(elemType))
             (cellSet,elemNames,line,f) = readElems(f,line,nodeID,elemDim,elemType,cellSet,order)
+            #print cellSet[cellName]['elemType'],'\n'
         elif line.startswith('*NSET'):
             sys.stdout.write( "Parsed {0} \n".format(line.strip()))
             (vertexSet,nodeNames,line,f) = readNodes(f,line,nodeID,vertexSet,nodeNames,order)
         else:
             line = f.readline()
     return coord,vertexSet,cellSet,elemNames,nodeNames
-	
+    
 def readCoords(f,line):
     #creating and filling list of coordinates from abaqus file
     coord = []
@@ -140,7 +144,7 @@ def readCoords(f,line):
     for i in range(len(nodeID)):
         order[nodeID[i]-1] = i+1
     return coord,nodeID,line,f,order 
-	
+    
 def readElems(f,line,nodeID,elemDim,elemType,cellSet,order):
     #creating and filling cellSet dictionary
     #get rid of word '*ELEMENT'
@@ -151,8 +155,8 @@ def readElems(f,line,nodeID,elemDim,elemType,cellSet,order):
         cellName = elemTmp[1].split('=')[1]
     else:
         cellName = elemTmp[0].split('=')[1]
-        cellType = elemTmp[1].split('=')[1]	
-    line = f.readline()		#continue to actual data		
+        cellType = elemTmp[1].split('=')[1] 
+    line = f.readline()     #continue to actual data        
     while not line.startswith('*') and not line == '':
         #get rid of index, this is connectivity table
         #this condition is for reading lines which have a comma at the end
@@ -173,14 +177,15 @@ def readElems(f,line,nodeID,elemDim,elemType,cellSet,order):
                 for i in range(len(tmp)):
                     cellConnect.append(tmp[i])
         else:
-            cellConnect = [order[int(x)-1] for x in line.strip().split(",")[1:]]	
+            cellConnect = [order[int(x)-1] for x in line.strip().split(",")[1:]]    
         ### cell sets:
         if not cellName in cellSet.keys():   #looking for tag in cellSet  
             cellSet[cellName] = {}           #creating new tag if doesn't exist
             cellSet[cellName]['connect'] = []
+            cellSet[cellName]['numVPE']  = elemDim[cellType]     #adding number of vertex per element to array
+            cellSet[cellName]['elemType'] = elemType[cellType]   #adding element type to array
+            print ("set type {0} converted to {1}\n".format(cellType,cellSet[cellName]['elemType']))
         cellSet[cellName]['connect'] += cellConnect  #reordering and adding to array
-        cellSet[cellName]['numVPE']  = elemDim[cellType]     #adding number of vertex per element to array
-        cellSet[cellName]['elemType'] = elemType[cellType]   #adding element type to array
         line = f.readline() 
 
     #create dictionary linking cellSet number to its name
@@ -232,42 +237,73 @@ def readNodes(f,line,nodeID,vertexSet,nodeNames,order):
         nodeNames[i] = vertNames[i]
     return vertexSet,nodeNames,line,f 
     
-	
+    
 #------Function for writing to exo format
-def exoWriter(coords,vertexSets,cellSets,filename,elemNames,nodeNames,numDims):
+def exoWriter(coords,vertexSets,cellSets,filename,elemNames,nodeNames):
+    cell1D = ("BAR","BAR2","BEAM2","BAR3","BEAM3")
+    cell2D = ("TRI","TRI3","TRIANGLE","TRISHELL","TRISHELL3","TRI6","TRISHELL6","QUAD","QUAD4","SHELL","SHELL4","QUAD9","SHELL9")
+    cell3D = ("TETRA","TETRA4","TETRA10","HEX","HEX8","HEX27")
     X = coords[:,0]         #set of all X coords
     Y = coords[:,1]         #set of all Y coords
     if len(coords[1])>2:
        Z = coords[:,2]         #set of all Z coords
+       numDim = 3
     else:                       #otherwise we assume we have a three dimensional case
        Z = [0]*len(X) 
+       numDim = 2
 
     numElem = 0
     for k in cellSets.keys():       #finding number of elements
         numElem += len(cellSets[k]['connect'])/cellSets[k]['numVPE']
 
     #setting up exo file for writing
-    e=exo.exodus(filename, mode='w',title='title',numDims=numDims,numNodes=len(X), 
+    e=exo.exodus(filename, mode='w',title='title',numDims=numDim,numNodes=len(X), 
                   numElems=numElem,numBlocks=len(cellSets),numNodeSets=len(vertexSets),numSideSets=0)
-    e.put_coord_names(["x","y","z"][0:numDims])    #name of each coordinate
+    e.put_coord_names(["x","y","z"][0:numDim])    #name of each coordinate
     e.put_coords(X,Y,Z)                 #actual coordinates
     
     print("\n")
     elemIDs = {}
     usedID = [0,]
-    for i in range(len(elemNames)):
+
+    #block info and connectivity
+    ###
+    ### Sort cell sets in order to write boundary cell sets last
+    ###
+    blocksOrder = []
+    c0blocks = []
+    c1blocks = []
+    if numDim == 2:
+        cellCoDim0 = cell2D
+        cellCoDim1 = cell1D
+    else:
+        cellCoDim0 = cell3D
+        cellCoDim1 = cell2D
+
+    for setID in cellSets.keys():
+        if cellSets[setID]['elemType'].upper() in cellCoDim0:
+            c0blocks.append(setID)
+    c0blocks.sort()
+    for setID in cellSets.keys():
+        if cellSets[setID]['elemType'].upper() in cellCoDim1:
+            c1blocks.append(setID)
+    c1blocks.sort()
+    blocksOrder = c0blocks + c1blocks
+        
+    for bn in blocksOrder:
         try:
-            setFixedID = int(elemNames[i])
+            setFixedID = int(bn)
             if setFixedID in usedID:
                 setFixedID =  max(usedID)+1    
             usedID.append(setFixedID)
         except ValueError:
             setFixedID =  max(usedID)+1
             usedID.append(setFixedID)
-        elemIDs[elemNames[i]] = setFixedID
-        print("Assigning ID {0:4d} to ELSET {1}. \tmef90/vDef name will be cs{0:04}".format(elemIDs[elemNames[i]],elemNames[i]))
+        elemIDs[bn] = setFixedID
+        print("Assigning ID {0:4d} to ELSET {1}. \tmef90/vDef name will be cs{0:04}".format(elemIDs[bn],bn))
+
     #block info and connectivity
-    for setID in cellSets.keys():
+    for setID in blocksOrder:#cellSets.keys():
         ###---setID, elemType, num elems, num nodes per elem, num attributes per elem
         e.put_elem_blk_info(elemIDs[setID],cellSets[setID]['elemType'],len(cellSets[setID]['connect'])/cellSets[setID]['numVPE'],cellSets[setID]['numVPE'],0)
         ###---setID, connectivity table
@@ -310,18 +346,22 @@ def Main():
     parser = argparse.ArgumentParser()
     parser.add_argument("abaqusFile", help = "The name of the ABAQUS file to be parsed.", type = str)
     parser.add_argument("exoFile", help = "The name of the exodus file to be written.", type = str)
-    parser.add_argument("--dim",type=int,help="mesh dimension",default=3)
-    parser.add_argument("--time_min",type=float,help="first time step",default=0.)
+    parser.add_argument("--force",action="store_true",default=False,help="Overwrite existing files without prompting")
+    parser.add_argument("--keepordering",action="store_true",default=False,help="Keep the original ELSET ordering")
+
+    #parser.add_argument("--time_min",type=float,help="first time step",default=0.)
     args = parser.parse_args()
-    if os.path.exists(args.exoFile):
-        if confirm("ExodusII file {0} already exists. Overwrite?".format(args.exoFile)):
+    if  os.path.exists(args.exoFile):
+        if args.force:
             os.remove(args.exoFile)
         else:
-            print "bye!"
-            sys.exit(-1)
-    
+            if confirm("ExodusII file {0} already exists. Overwrite?".format(args.exoFile)):
+                os.remove(args.exoFile)
+            else:
+                print '\n\t{0} was NOT generated from {1}\n'.format(args.exoFile,args.abaqusFile)
+                return -1
     (coord,vertexSet,cellSet,elemNames,nodeNames) = ABAQUSImporter(args.abaqusFile)
-    exoWriter(coord,vertexSet,cellSet,args.exoFile,elemNames,nodeNames,args.dim)
+    exoWriter(coord,vertexSet,cellSet,args.exoFile,elemNames,nodeNames)
 
 if __name__ == '__main__':
     Main()
