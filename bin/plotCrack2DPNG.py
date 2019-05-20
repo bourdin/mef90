52f517e2c083cfac4808b85dcbef4e42a32999f9
Blaise Bourdin <bourdin@lsu.edu>
2019-05-20 09:48:54 -0500

    Added basic PNG export script for 2D computations

---
+++ bin/plotCrack2DPNG.py
@@ -0,0 +1,230 @@
+from visit import *
+def savePNG(filename,geometry=[4096,4096]):
+    SaveWindowAtts = SaveWindowAttributes()
+    SaveWindowAtts.outputToCurrentDirectory = 1
+    SaveWindowAtts.fileName = filename
+    SaveWindowAtts.family = 0
+    SaveWindowAtts.format = SaveWindowAtts.PNG
+    SaveWindowAtts.width = geometry[0]
+    SaveWindowAtts.height = geometry[1]
+    SaveWindowAtts.resConstraint = SaveWindowAtts.NoConstraint
+    SaveWindowAtts.forceMerge = 1
+    SetSaveWindowAttributes(SaveWindowAtts)
+    return SaveWindow()
+
+def SetAnnotations():
+    import visit
+    AnnotationAtts = AnnotationAttributes()
+    AnnotationAtts.axes2D.visible = 1
+    AnnotationAtts.axes2D.autoSetTicks = 1
+    AnnotationAtts.axes2D.autoSetScaling = 1
+    AnnotationAtts.axes2D.lineWidth = 0
+    AnnotationAtts.axes2D.tickLocation = AnnotationAtts.axes2D.Outside  # Inside, Outside, Both
+    AnnotationAtts.axes2D.tickAxes = AnnotationAtts.axes2D.BottomLeft  # Off, Bottom, Left, BottomLeft, All
+    AnnotationAtts.axes2D.xAxis.title.visible = 0
+    AnnotationAtts.axes2D.xAxis.title.font.font = AnnotationAtts.axes2D.xAxis.title.font.Arial  # Arial, Courier, Times
+    AnnotationAtts.axes2D.xAxis.title.font.scale = 1
+    AnnotationAtts.axes2D.xAxis.title.font.useForegroundColor = 1
+    AnnotationAtts.axes2D.xAxis.title.font.color = (0, 0, 0, 255)
+    AnnotationAtts.axes2D.xAxis.title.font.bold = 0
+    AnnotationAtts.axes2D.xAxis.title.font.italic = 1
+    AnnotationAtts.axes2D.xAxis.title.userTitle = 0
+    AnnotationAtts.axes2D.xAxis.title.userUnits = 0
+    AnnotationAtts.axes2D.xAxis.title.title = "X-Axis"
+    AnnotationAtts.axes2D.xAxis.title.units = ""
+    AnnotationAtts.axes2D.xAxis.label.visible = 0
+    AnnotationAtts.axes2D.xAxis.label.font.font = AnnotationAtts.axes2D.xAxis.label.font.Arial  # Arial, Courier, Times
+    AnnotationAtts.axes2D.xAxis.label.font.scale = 1
+    AnnotationAtts.axes2D.xAxis.label.font.useForegroundColor = 1
+    AnnotationAtts.axes2D.xAxis.label.font.color = (0, 0, 0, 255)
+    AnnotationAtts.axes2D.xAxis.label.font.bold = 0
+    AnnotationAtts.axes2D.xAxis.label.font.italic = 1
+    AnnotationAtts.axes2D.xAxis.label.scaling = 0
+    AnnotationAtts.axes2D.xAxis.tickMarks.visible = 1
+    AnnotationAtts.axes2D.xAxis.tickMarks.majorMinimum = 0
+    AnnotationAtts.axes2D.xAxis.tickMarks.majorMaximum = 1
+    AnnotationAtts.axes2D.xAxis.tickMarks.minorSpacing = 0.02
+    AnnotationAtts.axes2D.xAxis.tickMarks.majorSpacing = 0.2
+    AnnotationAtts.axes2D.xAxis.grid = 0
+    AnnotationAtts.axes2D.yAxis.title.visible = 0
+    AnnotationAtts.axes2D.yAxis.title.font.font = AnnotationAtts.axes2D.yAxis.title.font.Arial  # Arial, Courier, Times
+    AnnotationAtts.axes2D.yAxis.title.font.scale = 1
+    AnnotationAtts.axes2D.yAxis.title.font.useForegroundColor = 1
+    AnnotationAtts.axes2D.yAxis.title.font.color = (0, 0, 0, 255)
+    AnnotationAtts.axes2D.yAxis.title.font.bold = 0
+    AnnotationAtts.axes2D.yAxis.title.font.italic = 1
+    AnnotationAtts.axes2D.yAxis.title.userTitle = 0
+    AnnotationAtts.axes2D.yAxis.title.userUnits = 0
+    AnnotationAtts.axes2D.yAxis.title.title = "Y-Axis"
+    AnnotationAtts.axes2D.yAxis.title.units = ""
+    AnnotationAtts.axes2D.yAxis.label.visible = 0
+    AnnotationAtts.axes2D.yAxis.label.font.font = AnnotationAtts.axes2D.yAxis.label.font.Arial  # Arial, Courier, Times
+    AnnotationAtts.axes2D.yAxis.label.font.scale = 1
+    AnnotationAtts.axes2D.yAxis.label.font.useForegroundColor = 1
+    AnnotationAtts.axes2D.yAxis.label.font.color = (0, 0, 0, 255)
+    AnnotationAtts.axes2D.yAxis.label.font.bold = 0
+    AnnotationAtts.axes2D.yAxis.label.font.italic = 1
+    AnnotationAtts.axes2D.yAxis.label.scaling = 0
+    AnnotationAtts.axes2D.yAxis.tickMarks.visible = 1
+    AnnotationAtts.axes2D.yAxis.tickMarks.majorMinimum = 0
+    AnnotationAtts.axes2D.yAxis.tickMarks.majorMaximum = 1
+    AnnotationAtts.axes2D.yAxis.tickMarks.minorSpacing = 0.02
+    AnnotationAtts.axes2D.yAxis.tickMarks.majorSpacing = 0.2
+    AnnotationAtts.axes2D.yAxis.grid = 0
+    AnnotationAtts.userInfoFlag = 0
+    AnnotationAtts.userInfoFont.font = AnnotationAtts.userInfoFont.Arial  # Arial, Courier, Times
+    AnnotationAtts.userInfoFont.scale = 1
+    AnnotationAtts.userInfoFont.useForegroundColor = 1
+    AnnotationAtts.userInfoFont.color = (0, 0, 0, 255)
+    AnnotationAtts.userInfoFont.bold = 0
+    AnnotationAtts.userInfoFont.italic = 0
+    AnnotationAtts.databaseInfoFlag = 0
+    AnnotationAtts.timeInfoFlag = 1
+    AnnotationAtts.databaseInfoFont.font = AnnotationAtts.databaseInfoFont.Arial  # Arial, Courier, Times
+    AnnotationAtts.databaseInfoFont.scale = 1
+    AnnotationAtts.databaseInfoFont.useForegroundColor = 1
+    AnnotationAtts.databaseInfoFont.color = (0, 0, 0, 255)
+    AnnotationAtts.databaseInfoFont.bold = 0
+    AnnotationAtts.databaseInfoFont.italic = 0
+    AnnotationAtts.databaseInfoExpansionMode = AnnotationAtts.File  # File, Directory, Full, Smart, SmartDirectory
+    AnnotationAtts.databaseInfoTimeScale = 1
+    AnnotationAtts.databaseInfoTimeOffset = 0
+    AnnotationAtts.legendInfoFlag = 0
+
+    visit.SetAnnotationAttributes(AnnotationAtts)
+    return 0
+
+def getlaststep(fname):
+  ### open file
+  f=open(fname)
+  ### Read last line in a string
+  lastline = f.readlines()[-1]
+  laststep = lastline.rsplit()[0] 
+  return(int(laststep))
+
+def drawCrack():
+    ##
+    ## Add pseudocolor plot of fracture field
+    ##
+
+    AddPlot('Pseudocolor', 'Damage')
+    PseudocolorAtts = PseudocolorAttributes()
+    PseudocolorAtts.scaling = PseudocolorAtts.Linear  # Linear, Log, Skew
+    PseudocolorAtts.skewFactor = 1
+    PseudocolorAtts.limitsMode = PseudocolorAtts.OriginalData  # OriginalData, CurrentPlot
+    PseudocolorAtts.minFlag = 1
+    PseudocolorAtts.min = 0
+    PseudocolorAtts.maxFlag = 1
+    PseudocolorAtts.max = 1
+    PseudocolorAtts.centering = PseudocolorAtts.Natural  # Natural, Nodal, Zonal
+    PseudocolorAtts.colorTableName = "hot"
+    PseudocolorAtts.invertColorTable = 0
+    PseudocolorAtts.opacityType = PseudocolorAtts.FullyOpaque  # ColorTable, FullyOpaque, Constant, Ramp, VariableRange
+    PseudocolorAtts.renderSurfaces = 1
+    PseudocolorAtts.renderWireframe = 0
+    PseudocolorAtts.renderPoints = 0
+    PseudocolorAtts.smoothingLevel = 0
+    PseudocolorAtts.legendFlag = 0
+    PseudocolorAtts.lightingFlag = 0
+    SetPlotOptions(PseudocolorAtts)
+
+    DrawPlots()
+    Query("SpatialExtents", use_actual_data=1)
+    BB = GetQueryOutputValue() 
+    SetView(BB)
+
+    return BB
+    
+
+def SetView(BB):
+    View2DAtts = View2DAttributes()
+    View2DAtts.viewportCoords = (0.05, 0.95, 0.05, 0.95)
+    View2DAtts.windowCoords = BB
+    View2DAtts.fullFrameActivationMode = View2DAtts.Auto  # On, Off, Auto
+    View2DAtts.fullFrameAutoThreshold = 100
+    View2DAtts.xScale = View2DAtts.LINEAR  # LINEAR, LOG
+    View2DAtts.yScale = View2DAtts.LINEAR  # LINEAR, LOG
+    View2DAtts.windowValid = 1
+    SetView2D(View2DAtts)
+    RecenterView()
+
+def setBGBlack():
+    AnnotationAtts = GetAnnotationAttributes()
+    AnnotationAtts.backgroundColor = (0, 0, 0, 255)
+    AnnotationAtts.foregroundColor = (255, 255, 255, 255)
+    SetAnnotationAttributes(AnnotationAtts)
+    return 0
+
+def setBGWhite():
+    AnnotationAtts = GetAnnotationAttributes()
+    AnnotationAtts.backgroundColor = (255, 255, 255, 255)
+    AnnotationAtts.foregroundColor = (0, 0, 0, 255)
+    SetAnnotationAttributes(AnnotationAtts)
+    return 0
+
+def plot(filename,step):
+    import json
+    import os
+    import os.path
+    import shutil
+    import math
+    
+    prefix,ext = os.path.splitext(filename)
+    enerfile = prefix+'.ener'
+
+    if step <= 0:
+        step = getlaststep(enerfile)
+
+    print("plotting step {0}".format(step))
+    ##  
+    ## Open the database
+    ##
+    MyDatabase = os.path.join(filename)
+      
+    print 'Trying to load {0}'.format(MyDatabase)
+    status = OpenDatabase(MyDatabase, step-1)       
+    if not status:
+        print "unable to open database %s"%MyDatabase
+        return -1
+
+    BB = drawCrack()
+    SetAnnotations()
+    DrawPlots()
+
+
+    W = BB[1]-BB[0]
+    H = BB[3]-BB[2]
+    if W > H:
+        geometry = (2048,int(2048.*H/W))
+    else:
+        geometry = (int(2048.*W/H),2048)
+    filenameW = '{basename}-{step:04d}-w'.format(basename = prefix,step=step)
+    setBGWhite()
+    status = savePNG(filenameW,geometry)
+    filenameB = '{basename}-{step:04d}-b'.format(basename = prefix,step=step)
+    setBGBlack()
+    status = savePNG(filenameB,geometry)
+
+    DeleteAllPlots()
+    CloseDatabase(MyDatabase)
+    return 0
+
+def parse(args=None):
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('inputfile',help='input file')
+    parser.add_argument('--step',type=int,default=-1)
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    import sys  
+    import os.path
+
+    options = parse()
+    if os.path.exists(options.inputfile):   
+        print('processing {0}'.format(options.inputfile)) 
+        plot(options.inputfile,options.step)   
+        sys.exit(0)
+    else:
+        sys.exit(-1)
+
