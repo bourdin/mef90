90e9e146bf7d0a67523df3def7e35a8d865c9119
Blaise Bourdin <bourdin@lsu.edu>
2020-01-28 22:47:58 -0600

    Reverted to an older version of exodus.py, rewrote exoshrink.py so that it does not need to load the whole file in memory

--- bin/exoShrink.py
+++ bin/exoShrink.py
@@ -2,11 +2,6 @@
 import argparse
 import sys
 import pymef90
-#try:
-#    import exomerge3 as exomerge
-#except:
-#    import exomerge2 as exomerge
-import exomerge
 
 
 def parse(args=None):
@@ -15,67 +10,92 @@ def parse(args=None):
     parser = argparse.ArgumentParser(description='Remove non-essential_fields from a .gen file.')
     parser.add_argument('inputfile',help='input file')
     parser.add_argument('-o','--outputfile',help='output file',default=None)
-    parser.add_argument('--field',help='list of variables to be deleted',nargs='*',default=[])
+    parser.add_argument('--remove',help='list of variables to be deleted',nargs='*',default=[])
     parser.add_argument("--force",action="store_true",default=False,help="Overwrite existing files without prompting")
     return parser.parse_args()
 
-def getFields(filename):
-    import exodus as exo
-    nodalFields = []
-    zonalFields = []
-    e = exo.exodus(filename) 
-    nodalFields = e.get_node_variable_names()
-    zonalFields = e.get_element_variable_names()
-    e.close()
-    return nodalFields,zonalFields
-
 def main():
+    import exodus as exo
+    import os
     options = parse()
-    nodalFields,zonalFields = getFields(options.inputfile)
 
-    print('nodal fields: {0}'.format(nodalFields))
 
-    deletedNodalFields = []
-    for f in nodalFields[::-1]:
-        for ff in options.field:
-            if f.lower().startswith(ff.lower()) and f not in deletedNodalFields:
-                deletedNodalFields.append(f)
+    exoin  = exo.exodus(options.inputfile,mode='r')
+    nodalFields = exoin.get_node_variable_names()
+    zonalFields = exoin.get_element_variable_names()
 
-    print('zonal fields: {0}'.format(zonalFields))
-    deletedZonalFields = []
-    for f in zonalFields[::-1]:
-        for ff in options.field:
-            if f.lower().startswith(ff.lower()) and f not in deletedZonalFields:
-                deletedZonalFields.append(f)
+    if options.remove == []:
+        print('nodal fields: {0}'.format(nodalFields))
+        print('zonal fields: {0}'.format(zonalFields))
 
-    if len(deletedNodalFields+deletedZonalFields) > 0:
-        print('You are about to delete the following fields:')
-        for f in deletedNodalFields[::-1]:
-            print('{0} (nodal)'.format(f))
-        for f in deletedZonalFields[::-1]:
-            print('{0} (zonal)'.format(f))
-        if not options.force:
-            if not pymef90.confirm("Is this OK?"):
-                print('Exiting without writing')
-                sys.exit()
+    keepNodalFields = nodalFields
+    for deleteField in options.remove:
+        keepNodalFields = [field for field in keepNodalFields if not field.lower().startswith(deleteField.lower())]
+    deleteNodalFields = [field for field in nodalFields if not field in keepNodalFields]
+
+    keepZonalFields = zonalFields
+    for deleteField in options.remove:
+        keepZonalFields = [field for field in keepZonalFields if not field.lower().startswith(deleteField.lower())]
+    deleteZonalFields = [field for field in zonalFields if not field in keepZonalFields]
+
+
+    if len(deleteNodalFields+deleteZonalFields) > 0:
+        print('\nDeleting the following fields:')
+        for f in deleteNodalFields:
+            print('\t{0} (nodal)'.format(f))
+        for f in deleteZonalFields:
+            print('\t{0} (zonal)'.format(f))
+
+        if options.outputfile == None or options.inputfile == options.outputfile:
+            if not options.force:
+                print('THIS WILL OVERWRITE INPUT FILE {0}'.format(options.inputfile))
+                if not pymef90.confirm("Is this OK?"):
+                    print('Exiting without writing')
+                    sys.exit()
+            outputfile = options.inputfile+'.tmp'
+        else:
+            if (os.path.exists(options.outputfile)):
+                if not options.force:
+                    print('THIS WILL OVERWRITE OUTPUT FILE {0}'.format(options.outputfile))
+                    if not pymef90.confirm("Is this OK?"):
+                        print('Exiting without writing')
+                        sys.exit()
+                if os.path.exists(options.outputfile):
+                    os.remove(options.outputfile)
+            outputfile = options.outputfile
+
+        print('inputfile: {0}'.format(options.inputfile))
+        print('outputfile: {0}'.format(outputfile))
+
+        print("Copying geometry in {0}".format(outputfile))
+        exoout = exoin.copy(outputfile)
+
+        print("formatting {0}".format(outputfile))
+        exoout.set_global_variable_number(0)
+        exoout.set_node_variable_number(len(keepNodalFields))
+        for i in range(len(keepNodalFields)):
+            exoout.put_node_variable_name(keepNodalFields[i],i+1)
+        exoout.set_element_variable_number(len(keepZonalFields))
+        for i in range(len(keepZonalFields)):
+            exoout.put_element_variable_name(keepZonalFields[i],i+1)
+        exoout.set_element_variable_truth_table([True] * exoout.numElemBlk.value * len(keepZonalFields))
+
+        step = 1
+        for t in exoin.get_times():
+            print("processing time step {0}: t={1:.2e}".format(step,t))
+            exoout.put_time(step,t)
+            for f in keepNodalFields:
+                exoout.put_node_variable_values(f,step,exoin.get_node_variable_values(f,step))
+            for f in keepZonalFields:
+                for cs in exoin.get_elem_blk_ids():
+                    exoout.put_element_variable_values(cs,f,step,exoin.get_element_variable_values(cs,f,step))
+
+            step += 1
+        exoin.close()
+        exoout.close()
 
         if options.outputfile == None:
-            outputfile = options.inputfile
-        else:
-            outputfile = options.outputfile
-        
-        print('loading model {0}. This may take a while.'.format(options.inputfile))
-        model = exomerge.import_model(options.inputfile)
-
-        for f in deletedZonalFields[::-1]:
-            model.delete_element_field(f)
-            print('\tdeleted {0}'.format(f))
-        for f in deletedNodalFields[::-1]:
-            model.delete_node_field(f)
-            print('\tdeleted {0}'.format(f))
-
-        print('Saving model. This may take a while')
-        model.export_model(outputfile)
+            os.rename(outputfile,options.inputfile)
     else:
         print('Nothing to delete')
     return 0
