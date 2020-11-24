31a9baf4a664893f6b1592455c594299af9d01fb
Blaise Bourdin <bourdin@lsu.edu>
2020-11-24 16:40:15 -0600

    Updated Dockerfiles to build with a more recent version of gcc

--- containers/getDependencies.sh
+++ containers/getDependencies.sh
@@ -5,15 +5,18 @@ else
 	mkdir tarball
 	cd tarball
 fi
-wget http://ftp.mcs.anl.gov/pub/petsc/externalpackages/Chaco-2.2.tar.gz
-wget https://github.com/gsjaardema/seacas/archive/b37ad5be658bd447717faa714f77ea3e4aa9537b.tar.gz
-wget http://ftp.mcs.anl.gov/pub/petsc/externalpackages/hypre-2.8.0b.tar.gz
-wget http://ftp.mcs.anl.gov/pub/petsc/externalpackages/ml-6.2-win.tar.gz
-wget https://www.unidata.ucar.edu/downloads/netcdf/ftp/netcdfc-4.7.4.tar.gz
-wget http://pyyaml.org/download/libyaml/yaml-0.1.4.tar.gz
-wget http://gmsh.info/bin/Linux/gmsh-4.5.6-Linux64.tgz
-#wget http://ftp.mcs.anl.gov/pub/petsc/externalpackages/boost_minimal_1_42_0.tar.gz
-#wget http://ftp.mcs.anl.gov/pub/petsc/externalpackages/sowing-1.1.16d.tar.gz
+
+echo 'Downloading netcdf  '; curl -O https://www.unidata.ucar.edu/downloads/netcdf/ftp/netcdf-c-4.7.4.tar.gz
+echo 'Downloading chaco   '; curl -O http://ftp.mcs.anl.gov/pub/petsc/externalpackages/Chaco-2.2.tar.gz
+echo 'Downloading exodusii'; curl -O https://github.com/gsjaardema/seacas/archive/0ac6baa46262ab17a7b3c0a07b8832ce8d088324.tar.gz
+echo 'Downloading yaml    '; curl -O http://pyyaml.org/download/libyaml/yaml-0.1.4.tar.gz
+echo 'Downloading hypre   '; curl -O http://ftp.mcs.anl.gov/pub/petsc/externalpackages/hypre-2.8.0b.tar.gz
+echo 'Downloading sowing  '; curl -O http://ftp.mcs.anl.gov/pub/petsc/externalpackages/sowing-1.1.26-p1.tar.gz
+echo 'Downloading ml      '; curl -O http://ftp.mcs.anl.gov/pub/petsc/externalpackages/ml-6.2-p3.tar.gz
+echo 'Downloading parmetis'; curl -O http://ftp.mcs.anl.gov/pub/petsc/externalpackages/parmetis-4.0.2-p3.tar.gz
+echo 'Downloading hdf5    '; curl -O http://ftp.mcs.anl.gov/pub/petsc/externalpackages/hdf5-1.8.8-p1.tar.gz
+echo 'Downloading metis   '; curl -O http://ftp.mcs.anl.gov/pub/petsc/externalpackages/metis-5.0.2-p3.tar.gz
+#echo 'Downloading gmsh    '; curl -O http://gmsh.info/bin/Linux/gmsh-4.7.1-Linux64.tgz
 
 if [ -d petsc-3.3-mef90 ]; then
 	cd petsc-3.3-mef90
