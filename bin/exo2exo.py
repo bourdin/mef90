613beefde92b5a7cfdba6c4091ba542506d68835
Jean-Michel Scherer <jean-michel.scherer@enpc.fr>
2022-06-22 11:23:38 -0700

    make prints compatible with python3

--- bin/exo2exo.py
+++ bin/exo2exo.py
@@ -7,6 +7,7 @@ else:
 import os.path
 import numpy as np
 import pymef90
+import ctypes
 
 
 def exo2exo(fin,fout):
@@ -18,7 +19,7 @@ def exo2exo(fin,fout):
     cell2D = ("TRI","TRI3","TRIANGLE","TRISHELL","TRISHELL3","TRI6","TRISHELL6","QUAD","QUAD4","SHELL","SHELL4","QUAD9","SHELL9")
     cell3D = ("TETRA","TETRA4","TETRA10","HEX","HEX8","HEX27")
 
-    print "Opening {0}".format(fin)
+    print("Opening {0}".format(fin))
     e = exo.exodus(fin,"r")
     dim       = e.num_dimensions()
     nVertices = e.num_nodes()
@@ -26,18 +27,18 @@ def exo2exo(fin,fout):
     nCSets    = e.num_blks()
     nVSets    = e.num_node_sets()
 
-    print "\tnumber of dimensions: {0}".format(dim)
-    print "\tnumber of vertices: {0}".format(nVertices)
-    print "\tnumber of cells: {0}".format(nCells)
-    print "\tnumber of cell sets: {0}".format(nCSets)
-    print "\tnumber of vertex sets: {0}".format(nVSets)
+    print( "\tnumber of dimensions: {0}".format(dim))
+    print( "\tnumber of vertices: {0}".format(nVertices))
+    print( "\tnumber of cells: {0}".format(nCells))
+    print( "\tnumber of cell sets: {0}".format(nCSets))
+    print( "\tnumber of vertex sets: {0}".format(nVSets))
 
 
 
     ### Reorder cell sets 
     ### List cells of coDIm0 first then cells of coDim 1
     ###
-    print "Reordering cell sets by increasing co-dimension"
+    print( "Reordering cell sets by increasing co-dimension")
     cellsType = []
     for set in range(nCSets):
         setID = e.get_elem_blk_ids()[set]
@@ -63,7 +64,7 @@ def exo2exo(fin,fout):
     ### Find hanging nodes
     ###
     ### 1. find largest vertex index in all connectivity tables
-    print "Removing hanging nodes"
+    print( "Removing hanging nodes")
     minV = 100000000
     maxV = 0
     for set in range(nCSets):
@@ -72,7 +73,7 @@ def exo2exo(fin,fout):
         for v in connect[0]:
             maxV = max(maxV,v)
             minV = min(minV,v)
-    print "\tVertex range: {0}/{1}".format(minV,maxV)
+    print( "\tVertex range: {0}/{1}".format(minV,maxV))
 
     listedVertices = [False,]*maxV
     for set in range(nCSets):
@@ -85,7 +86,7 @@ def exo2exo(fin,fout):
     vertexReordering = np.array(range(maxV),dtype=int)
     for v in range(len(listedVertices)):
         if not listedVertices[v]:
-            print "\tvertex {0} is missing.".format(v)
+            print( "\tvertex {0} is missing.".format(v))
             vertexReordering[v:] = vertexReordering[v:]-1
             vertexReordering[v]  = -1
             numMissing += 1
@@ -100,9 +101,9 @@ def exo2exo(fin,fout):
     ###
     X,Y,Z=e.get_coords()
     eout.put_coord_names(["x","y","z"][0:dim])
-    fixedX = np.empty(nVertices,dtype=exo.c_double)
-    fixedY = np.empty(nVertices,dtype=exo.c_double)
-    fixedZ = np.empty(nVertices,dtype=exo.c_double)
+    fixedX = np.empty(nVertices,dtype=ctypes.c_double) #exo.c_double)
+    fixedY = np.empty(nVertices,dtype=ctypes.c_double) #exo.c_double)
+    fixedZ = np.empty(nVertices,dtype=ctypes.c_double) #exo.c_double)
 
     for i in range(nVertices):
         if listedVertices[i]:
@@ -128,9 +129,9 @@ def exo2exo(fin,fout):
             usedID.append(setFixedID)
         cellType,numCells,numVertexPerCell,numAttr = e.elem_blk_info(setID)
         print('Assigning ID {0:4d} to cell set "{1}". \tmef90/vDef name will be cs{0:04}'.format(setFixedID,setName))
-        print "\tNumber of cells: {0}".format(numCells)
-        print "\tCell type: {0}".format(cellType)
-        print "\tNumber of vertex per cell: {0}".format(numVertexPerCell)
+        print( "\tNumber of cells: {0}".format(numCells))
+        print( "\tCell type: {0}".format(cellType))
+        print( "\tNumber of vertex per cell: {0}".format(numVertexPerCell))
 
         eout.put_elem_blk_info(setFixedID,cellType,numCells,numVertexPerCell,1)
         eout.put_elem_blk_name(setFixedID,setName)
@@ -177,18 +178,18 @@ def exo2exo(fin,fout):
 
 if __name__ == '__main__':
     if not (len(sys.argv) == 3):
-        print "usage: {0} <input filename> <output filename>".format(sys.argv[0])
+        print("usage: {0} <input filename> <output filename>".format(sys.argv[0]))
         sys.exit(-1)
     fin = sys.argv[-2]
     fout = sys.argv[-1]
     if not os.path.isfile(fin):
-        print "usage: {0} <input filename> <output filename>".format(sys.argv[0])
+        print( "usage: {0} <input filename> <output filename>".format(sys.argv[0]))
         sys.exit(-1)
     if os.path.exists(fout):
         if pymef90.confirm("ExodusII file {0} already exists. Overwrite?".format(fout)):
             os.remove(fout)
         else:
-            print "bye!"
+            print( "bye!")
             sys.exit(-1)
 
     exo2exo(fin,fout)
