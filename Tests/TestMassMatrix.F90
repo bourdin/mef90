bf3d66dd83c0011c3afbcace4d9edd72822597d8
Blaise Bourdin <bourdin@mcmaster.ca>
2022-10-28 14:25:12 -0400

    Updated TestMassMatrix

--- Tests/TestMassMatrix.F90
+++ Tests/TestMassMatrix.F90
@@ -4,28 +4,29 @@ Program  TestMassMatrix
     Use petsc
     Implicit NONE   
         
-    PetscErrorCode                      :: ierr
-    Type(MEF90Ctx_Type),target          :: MEF90Ctx
-    Type(MEF90CtxGlobalOptions_Type)    :: MEF90GlobalOptions_default
-    Type(tDM)                           :: dm
-    PetscBool                           :: interpolate = PETSC_TRUE, hasConstraints = PETSC_FALSE
-    Character(len=MEF90MXSTRLEN)       :: IOBuffer
-    PetscEnum                           :: setType
+    PetscErrorCode                            :: ierr
+    Type(MEF90Ctx_Type),target                :: MEF90Ctx
+    Type(MEF90CtxGlobalOptions_Type)          :: MEF90GlobalOptions_default
+    Type(tDM)                                 :: dm
+    PetscBool                                 :: interpolate = PETSC_TRUE, hasConstraints = PETSC_FALSE
+    Character(len=MEF90MXSTRLEN)              :: IOBuffer,name
+    PetscEnum                                 :: setType
 
-    PetscInt                            :: numComponents
-    PetscInt                            :: set
-    Type(MEF90ElementType)             :: cellSetElementType,faceSetElementType
-    type(tIS)                           :: setIS
-    PetscInt,Dimension(:),pointer       :: setID
-    PetscInt                            :: dim,pStart,pEnd,order = 1
-    PetscBool                           :: flg
-    Type(tPetscSection)                 :: section
-    Logical,Dimension(:,:),Pointer      :: ConstraintTruthTableU
-    Logical,Dimension(:),Pointer        :: Constraints
-    Type(tMat)                          :: M
+    PetscInt                                  :: numComponents
+    PetscInt                                  :: set
+    Type(MEF90ElementType)                    :: cellSetElementType,faceSetElementType
+    type(tIS)                                 :: setIS
+    PetscInt,Dimension(:),pointer             :: setID
+    PetscInt                                  :: dim,pStart,pEnd,order = 1
+    PetscBool                                 :: flg
+    Type(tPetscSection)                       :: section
+    Logical,Dimension(:,:),Pointer            :: ConstraintTruthTableU
+    Logical,Dimension(:),Pointer              :: Constraints
+    Type(tVec)                                :: U,U0
+    Type(tDM)                                 :: dmU,dmU0
+    Type(tMat)                                :: M,M0
+    Type(MEF90CtxGlobalOptions_Type),pointer  :: MEF90GlobalOptions
 
-    PetscCallA(PetscInitialize(PETSC_NULL_CHARACTER,ierr))
-    Call MEF90Initialize(ierr)
 
     MEF90GlobalOptions_default%verbose           = 1
     MEF90GlobalOptions_default%dryrun            = PETSC_FALSE
@@ -37,177 +38,143 @@ Program  TestMassMatrix
     MEF90GlobalOptions_default%timeNumCycle      = 1
     MEF90GlobalOptions_default%elementFamily     = MEF90ElementFamilyLagrange
     MEF90GlobalOptions_default%elementOrder      = 1
-
-    Call MEF90CtxCreate(PETSC_COMM_WORLD,MEF90Ctx,MEF90GlobalOptions_default,ierr)
+ 
+    PetscCallA(PetscInitialize(PETSC_NULL_CHARACTER,ierr))
     
-    PetscCallA(PetscPrintf(MEF90Ctx%Comm,MEF90Ctx%geometryfile//'\n',ierr))
+    Call MEF90Initialize(ierr)
+    Call MEF90CtxCreate(PETSC_COMM_WORLD,MEF90Ctx,MEF90GlobalOptions_default,ierr)
+    PetscCallA(PetscBagGetDataMEF90CtxGlobalOptions(MEF90Ctx%GlobalOptionsBag,MEF90GlobalOptions,ierr))    
+
     PetscCallA(DMPlexCreateFromFile(MEF90Ctx%Comm,MEF90Ctx%geometryfile,PETSC_NULL_CHARACTER,interpolate,dm,ierr))
     PetscCallA(DMPlexDistributeSetDefault(dm,PETSC_FALSE,ierr))
     PetscCallA(DMSetFromOptions(dm,ierr))
-    PetscCallA(DMViewFromOptions(dm,PETSC_NULL_OPTIONS,"-dm_view",ierr))
-    
     distribute: Block 
         Type(tDM),target                    :: dmDist
+        PetscInt                            :: ovlp = 0
         If (MEF90Ctx%NumProcs > 1) Then
-            PetscCallA(DMPlexDistribute(dm,0,PETSC_NULL_SF,dmDist,ierr))
+            PetscCallA(DMPlexDistribute(dm,ovlp,PETSC_NULL_SF,dmDist,ierr))
             PetscCallA(DMDestroy(dm,ierr))
             dm = dmDist
-        end If
-    end Block distribute
+        End If
+    End Block distribute
     PetscCallA(DMViewFromOptions(dm,PETSC_NULL_OPTIONS,"-mef90dm_view",ierr))
 
     PetscCallA(DMGetDimension(dm,dim,ierr))
-    PetscCallA(DMPlexGetChart(dm,pStart,pEnd,ierr))
 
-    PetscCallA(PetscSectionCreate(MEF90Ctx%Comm,section,ierr))
-    PetscCallA(PetscObjectSetName(section,"Section for U",ierr))
-    PetscCallA(PetscSectionSetChart(section,pStart,pEnd,ierr))
+    name = "U"
+    PetscCallA(MEF90CreateLocalVector(dm,MEF90GlobalOptions%elementFamily,MEF90GlobalOptions%elementOrder,1_Ki,name,U,ierr))
+    name = "U0"
+    PetscCallA(MEF90CreateBoundaryLocalVector(dm,MEF90GlobalOptions%elementFamily,MEF90GlobalOptions%elementOrder,1_Ki,name,U0,ierr))
 
+    PetscCallA(VecGetDM(U,dmU,ierr))
+    PetscCallA(VecGetDM(U0,dmU0,ierr))
 
-    numComponents = dim
-    !!! Allocate DoF at cell and face sets
-    !!! Note that if the face sets corresponds to faces in elements in cell set 
-    !!! (which will always be the case in an exodusII mesh), the second call does nothing
-    PetscCallA(PetscOptionsGetInt(PETSC_NULL_OPTIONS,'','-order',order,flg,ierr))
-    If (dim == 2) Then
-        Select case(order)
-        case(1)
-            cellSetElementType = MEF90P1Lagrange2D
-            faceSetElementType = MEF90_P1_Lagrange_2DBoundary
-        case(2)
-            cellSetElementType = MEF90_P2_Lagrange_2D
-            faceSetElementType = MEF90_P2_Lagrange_2DBoundary
-        Case default
-            Write(IOBuffer,*) 'ERROR: unimplemented order ', order, '\n'
-            SETERRA(MEF90Ctx%Comm,PETSC_ERR_USER,IOBuffer)
-        End Select
-    Else If (dim == 3) Then
-        Select case(order)
-            case(1)
-                cellSetElementType = MEF90_P1_Lagrange_3D
-                faceSetElementType = MEF90_P1_Lagrange_3DBoundary
-            case(2)
-                cellSetElementType = MEF90_P2_Lagrange_3D
-                faceSetElementType = MEF90_P2_Lagrange_3DBoundary
-            Case default
-            Write(IOBuffer,*) 'ERROR: unimplemented order ', order, '\n'
-            SETERRA(MEF90Ctx%Comm,PETSC_ERR_USER,IOBuffer)
-        End Select
-    End If
-    PetscCallA(MEF90_SectionAllocateDof(dm,MEF90_DMPlexcellSetType,cellSetElementType,numComponents,section,ierr))
-    PetscCallA(MEF90_SectionAllocateDof(dm,MEF90_DMPlexfaceSetType,faceSetElementType,numComponents,section,ierr))
+    PetscCallA(DMCreateMatrix(dmU,M,iErr))
 
-
-    PetscCallA(PetscOptionsGetBool(PETSC_NULL_OPTIONS,'','-constraint',hasConstraints,flg,ierr))
-    If (hasConstraints) Then
-
-        !!! Allocate constraints.
-        !!! This is definitely not optimized. We could create a table of dimension
-        !!! # points with dof x # components instead of #points x # components
-        !!! We can address this later if needed
-        !!! The whole constraint setup takes 2 passes: 
-        !!!   1. Fill the constraint truth table (typically from data in CS/FS/ES/VS bag)
-        !!!      This is done in MEF90_SetupConstraintTableSet
-        !!!   2. Allocate space in the section, call PetscSectionSetup, and set the constraint indices
-        !!!      for each constrained dof. This is done in MEF90_SectionAllocateConstraint
-        PetscCallA(DMPlexGetChart(dm,pStart,pEnd,ierr))
-        Allocate(ConstraintTruthTableU(pEnd,numComponents))
-        ConstraintTruthTableU  = .FALSE.
-
-        Allocate(constraints(numComponents))
-        constraints = .FALSE.
-
-        setType = MEF90_DMPlexfaceSetType
-        PetscCallA(DMGetLabelIdIS(dm,MEF90_DMPlexSetLabelName(setType),setIS,ierr))
-        PetscCallA(ISGetIndicesF90(setIS,setID,ierr))
-        Do set = 1,size(setID)
-            !!! setting the constrained components to an arbitrary value
-            !!! In real life, we would get constraint from the CS/FS/ES/VS bag
-            constraints = .FALSE.
-            constraints(mod(setID(set),numComponents)+1) = .TRUE.
-            PetscCallA(MEF90_SetupConstraintTableSet(dm,section,setType,setID(set),constraints,ConstraintTruthTableU,ierr))
-        End Do
-        PetscCallA(ISRestoreIndicesF90(setIS,setID,ierr))
-        PetscCallA(ISDestroy(setIS,ierr))
-
-        setType = MEF90_DMPlexVertexSetType
-        PetscCallA(DMGetLabelIdIS(dm,MEF90_DMPlexSetLabelName(setType),setIS,ierr))
-        PetscCallA(ISGetIndicesF90(setIS,setID,ierr))
-        Do set = 1,size(setID)
-            !!! setting the constrained components to an arbitrary value
-            !!! In real life, we would get constraint from the CS/FS/ES/VS bag
-            constraints = .FALSE.
-            constraints(mod(setID(set),numComponents)+1) = .TRUE.
-            PetscCallA(MEF90_SetupConstraintTableSet(dm,section,setType,setID(set),constraints,ConstraintTruthTableU,ierr))
-        End Do
-        PetscCallA(ISRestoreIndicesF90(setIS,setID,ierr))
-        PetscCallA(ISDestroy(setIS,ierr))
-        DeAllocate(constraints)
-
-        PetscCallA(MEF90_SectionAllocateConstraint(dm,ConstraintTruthTableU,section,ierr))
-
-        DeAllocate(ConstraintTruthTableU)
-    Else
-        PetscCallA(PetscSectionSetup(section,ierr))
-    End If
-    PetscCallA(PetscSectionViewFromOptions(section,PETSC_NULL_OPTIONS,"-mef90section_view",ierr))
-
-    PetscCallA(DMSetLocalSection(dm,section,ierr))
-    PetscCallA(DMCreateMatrix(dm,M,ierr))
-
-    testClosure: Block
-        PetscScalar,dimension(:),Pointer :: velem
-        Type(tVec)                       :: v
-        PetscInt                         :: point = 0
-
-        PetscCallA(DMCreateGlobalVector(dm,v,ierr))
-        PetscCallA(DMPlexVecGetClosure(dm,section,v,point,velem,ierr))
-        write(*,*)'---', velem
-        PetscCallA(DMPlexVecrestoreClosure(dm,section,v,point,velem,ierr))
-        PetscCallA(VecDestroy(v,ierr))
-
-    end block testClosure
-
+    PetscCallA(DMCreateMatrix(dmU0,M0,iErr))
 
     elementCreate: Block
         Type(MEF90Element2DVect),dimension(:),Pointer  :: elem2D
         Type(MEF90Element3DVect),dimension(:),Pointer  :: elem3D
-        PetscInt                                       :: quadratureOrder = 2
+        PetscInt                                       :: quadratureOrder
         Type(tIS)                                      :: setPointIS
+        PetscInt,Dimension(:),Pointer                  :: setPointID
+        Type(MEF90ElementType)                         :: elementType
+        DMPolytopeType                                 :: cellType
 
-        setType = MEF90_DMPlexcellSetType
-        PetscCallA(DMGetLabelIdIS(dm,MEF90_DMPlexSetLabelName(setType),setIS,ierr))
+        setType = MEF90CellSetType
+        PetscCallA(DMGetLabelIdIS(dmU,MEF90SetLabelName(setType),setIS,ierr))
         If (setIS /= PETSC_NULL_IS) Then
             PetscCallA(ISGetIndicesF90(setIS,setID,ierr))
+            QuadratureOrder = elementType%order * 2
             Do set = 1,size(setID)
-                PetscCallA(DMGetStratumIS(dm,MEF90_DMPlexSetLabelName(setType),setID(set),setPointIS,ierr))
-                If (dim == 2) Then
-                    PetscCallA(MEF90ElementCreate(dm,setPointIS,elem2D,QuadratureOrder,cellSetElementType,ierr))
-                    PetscCallA(MEF90_MassMatrixAssembleSet(M,dm,setType,setID(set),elem2D,cellSetElementType,ierr))
+                PetscCallA(DMGetStratumIS(dmU,MEF90SetLabelName(setType),setID(set),setPointIS,ierr))
+                PetscCallA(ISGetIndicesF90(setPointIS,setPointID,ierr))
+                PetscCall(DMPlexGetCellType(dmU,setPointID(1),cellType,ierr))
+                PetscCall(MEF90ElementGetType(MEF90GlobalOptions_default%elementFamily,MEF90GlobalOptions_default%elementOrder,cellType,elementType,ierr))
+                    If (dim == 2) Then
+                    PetscCallA(MEF90ElementCreate(dmU,setPointIS,elem2D,QuadratureOrder,elementType,ierr))
+                    PetscCallA(MEF90_MassMatrixAssembleSet(M,dmU,setType,setID(set),elem2D,elementType,ierr))
                     PetscCallA(MEF90ElementDestroy(elem2D,ierr))
                 Else
-                    PetscCallA(MEF90ElementCreate(dm,setPointIS,elem3D,QuadratureOrder,cellSetElementType,ierr))
-                    PetscCallA(MEF90_MassMatrixAssembleSet(M,dm,setType,setID(set),elem3D,cellSetElementType,ierr))
+                    PetscCallA(MEF90ElementCreate(dmU,setPointIS,elem3D,QuadratureOrder,elementType,ierr))
+                    PetscCallA(MEF90_MassMatrixAssembleSet(M,dmU,setType,setID(set),elem3D,elementType,ierr))
                     PetscCallA(MEF90ElementDestroy(elem3D,ierr))
                 End If
+                PetscCallA(ISRestoreIndicesF90(setPointIS,setPointID,ierr))
                 PetscCallA(ISDestroy(setPointIS,ierr))
             End Do
             PetscCallA(ISRestoreIndicesF90(setIS,setID,ierr))
         End If ! setIS
         PetscCallA(ISDestroy(setIS,ierr))
-    End Block elementCreate
-    PetscCallA(MatAssemblyBegin(M,MAT_FINAL_ASSEMBLY,ierr))
-    PetscCallA(MatAssemblyEnd(M,MAT_FINAL_ASSEMBLY,ierr))
+        PetscCallA(MatAssemblyBegin(M,MAT_FINAL_ASSEMBLY,ierr))
+        PetscCallA(MatAssemblyEnd(M,MAT_FINAL_ASSEMBLY,ierr))
+        PetscCallA(MatViewFromOptions(M,PETSC_NULL_OPTIONS,"-mef90matM_view",ierr))
 
-    PetscCallA(MatViewFromOptions(M,PETSC_NULL_OPTIONS,"-mef90mat_view",ierr))
+        setType = MEF90FaceSetType
+        PetscCallA(DMGetLabelIdIS(dmU0,MEF90SetLabelName(setType),setIS,ierr))
+        If (setIS /= PETSC_NULL_IS) Then
+            PetscCallA(ISGetIndicesF90(setIS,setID,ierr))
+            QuadratureOrder = elementType%order * 2
+            Do set = 1,size(setID)
+                PetscCallA(DMGetStratumIS(dmU0,MEF90SetLabelName(setType),setID(set),setPointIS,ierr))
+                PetscCallA(ISGetIndicesF90(setPointIS,setPointID,ierr))
+                PetscCall(DMPlexGetCellType(dmU0,setPointID(1),cellType,ierr))
+                PetscCall(MEF90ElementGetTypeBoundary(MEF90GlobalOptions_default%elementFamily,MEF90GlobalOptions_default%elementOrder,cellType,elementType,ierr))
+                    If (dim == 2) Then
+                    PetscCallA(MEF90ElementCreate(dmU0,setPointIS,elem2D,QuadratureOrder,elementType,ierr))
+                    PetscCallA(MEF90_MassMatrixAssembleSet(M0,dmU0,setType,setID(set),elem2D,elementType,ierr))
+                    PetscCallA(MEF90ElementDestroy(elem2D,ierr))
+                Else
+                    PetscCallA(MEF90ElementCreate(dmU0,setPointIS,elem3D,QuadratureOrder,elementType,ierr))
+                    PetscCallA(MEF90_MassMatrixAssembleSet(M,dmU0,setType,setID(set),elem3D,elementType,ierr))
+                    PetscCallA(MEF90ElementDestroy(elem3D,ierr))
+                End If
+                PetscCallA(ISRestoreIndicesF90(setPointIS,setPointID,ierr))
+                PetscCallA(ISDestroy(setPointIS,ierr))
+            End Do
+            PetscCallA(ISRestoreIndicesF90(setIS,setID,ierr))
+        End If ! setIS
+        PetscCallA(ISDestroy(setIS,ierr))
+        PetscCallA(MatAssemblyBegin(M0,MAT_FINAL_ASSEMBLY,ierr))
+        PetscCallA(MatAssemblyEnd(M0,MAT_FINAL_ASSEMBLY,ierr))
+        PetscCallA(MatViewFromOptions(M0,PETSC_NULL_OPTIONS,"-mef90matM0_view",ierr))
+    End Block elementCreate
+
+
+    TestMass: Block
+        Type(tVec)                 :: Uglob,MUglob
+        PetscReal                  :: Mass
+
+        PetscCallA(dmCreateGlobalVector(dmU,Uglob,ierr))
+        PetscCallA(VecDuplicate(Uglob,MUglob,ierr))
+        PetscCallA(VecSet(UGlob,1.0_Kr,ierr))
+        PetscCallA(MatMult(M,Uglob,MUglob,ierr))
+        PetscCallA(VecDot(UGlob,MUGlob,mass,ierr))
+        Write(IOBuffer,'("Total mass", ES12.5," \n")') mass
+        PetscCallA(PetscPrintf(PETSC_COMM_WORLD,IOBuffer, ierr))
+        PetscCallA(VecDestroy(Uglob,ierr))
+        PetscCallA(VecDestroy(MUglob,ierr))
+
+        PetscCallA(dmCreateGlobalVector(dmU0,Uglob,ierr))
+        PetscCallA(VecDuplicate(Uglob,MUglob,ierr))
+        PetscCallA(VecSet(UGlob,1.0_Kr,ierr))
+        PetscCallA(MatMult(M0,Uglob,MUglob,ierr))
+        PetscCallA(VecDot(UGlob,MUGlob,mass,ierr))
+        Write(IOBuffer,'("Boundary mass", ES12.5," \n")') mass
+        PetscCallA(PetscPrintf(PETSC_COMM_WORLD,IOBuffer, ierr))
+        PetscCallA(VecDestroy(Uglob,ierr))
+        PetscCallA(VecDestroy(MUglob,ierr))
+    End Block TestMass
 
     PetscCallA(MatDestroy(M,ierr))
-    !PetscCallA(PetscSectionDestroy(section,ierr))
-    PetscCallA(DMDestroy(dm,ierr))
+    PetscCallA(MatDestroy(M0,ierr))
+    PetscCallA(VecDestroy(U,ierr))
+    PetscCallA(VecDestroy(U0,ierr))
 
     Call MEF90CtxDestroy(MEF90Ctx,ierr)   
     Call MEF90Finalize(ierr)
-    Call PetscFinalize(ierr)
+    PetscCallA(PetscFinalize(ierr))
 End Program TestMassMatrix
            
     
